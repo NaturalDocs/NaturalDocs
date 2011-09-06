36876b34622375a0ebdf1b1e91915e523e1d020c
Greg Valure <gregvalure@naturaldocs.org>
2011-09-06 19:08:51 -0400

    Update tests and results for parameter matching.

--- EngineTests/Test Data/HTML Generation/Parameter Lists/Identifier Symbols - Input.pl
+++ EngineTests/Test Data/HTML Generation/Parameter Lists/Identifier Symbols - Input.pl
@@ -4,9 +4,26 @@
 #	Fake Perl syntax but that's not important.
 #	
 #	Parameters:
-#		x - Should match without the symbol
+#		$x - Should match with the symbol
 #		@y - Should match with the symbol
+#		%z - Should match with the symbol
 #
-sub FunctionName ($x, @y, %z)
+#	Parameters:
+#		x - Should match without the symbol
+#		y - Should match without the symbol
+#		z - Should match without the symbol
+#
+sub FunctionName (int $x, array @y, table %z)
+	{
+	}
+	
+# Function: FunctionName2
+#
+#	Parameters:
+#		x - Should not show the type if it's just a symbol
+#		y - Should not show the type if it's just a symbol
+#		z - Should not show the type if it's just a symbol
+#
+sub FunctionName2 ($x, @y, %z)
 	{
 	}
