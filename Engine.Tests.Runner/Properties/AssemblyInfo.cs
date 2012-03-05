a9c936cbfac1b5113329df3bfb5590b70c21d701
Greg Valure <gregvalure@naturaldocs.org>
2012-03-05 13:32:42 -0500

    Create a simple test runner project to allow NUnit tests to be debugged.

---
+++ Engine.Tests.Runner/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Natural Docs Engine NUnit Test Runner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Greg Valure")]
+[assembly: AssemblyProduct("Natural Docs")]
+[assembly: AssemblyCopyright("Copyright © 2003-2012 Greg Valure")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4e3740ae-315b-49a1-affd-b282872b1fdd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Engine.Tests.Runner/Runner.cs
@@ -0,0 +1,30 @@
+﻿/* 
+ * Class: GregValure.NaturalDocs.Engine.Tests.Runner
+ * ____________________________________________________________________________
+ * 
+ * A simple class to run NUnit tests from a Visual Studio console project which allows you to set breakpoints
+ * in the tests and debug them.
+ * 
+ */
+
+// This file is part of Natural Docs, which is Copyright © 2003-2012 Greg Valure.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+
+namespace GregValure.NaturalDocs.Engine.Tests
+	{
+	class Runner
+		{
+		static void Main (string[] args)
+			{
+			NUnit.ConsoleRunner.Runner.Main(args);
+
+			System.Console.WriteLine();
+			System.Console.WriteLine("Press any key to continue...");
+			System.Console.ReadKey(false);
+			}
+		}
+	}
