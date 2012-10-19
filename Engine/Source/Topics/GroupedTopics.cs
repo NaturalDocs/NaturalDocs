3a2f2ebaae396fe8b59e5a0c529c41668b0b5b2e
Greg Valure <gregvalure@naturaldocs.org>
2012-10-19 11:28:57 -0400

    Add classes to support working with topics in groups.

---
+++ Engine/Source/Topics/GroupedTopics.cs
@@ -0,0 +1,235 @@
+﻿/* 
+ * Class: GregValure.NaturalDocs.Engine.Topics.GroupedTopics
+ * ____________________________________________________________________________
+ * 
+ * A list of <Topics> divided into <TopicGroups>.
+ */
+
+// This file is part of Natural Docs, which is Copyright © 2003-2012 Greg Valure.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+using System.Collections.Generic;
+
+
+namespace GregValure.NaturalDocs.Engine.Topics
+	{
+	public class GroupedTopics
+		{
+
+		// Group: Functions
+		// __________________________________________________________________________
+
+
+		/* Constructor: GroupedTopics
+		 * Creates a new list with the passed topics and no grouping applied.
+		 */
+		public GroupedTopics (List<Topic> topics)
+			{
+			this.topics = topics;
+			this.groups = new List<TopicGroup>();
+			}
+
+
+		/* Function: CreateGroup
+		 * Creates a grouping in the topic list.  Groups do not have to encompass every topic but they must be created 
+		 * in order and must not overlap.
+		 */
+		public void CreateGroup (int startingIndex, int count)
+			{
+			#if DEBUG
+			if (groups.Count > 0)
+				{
+				var lastGroup = groups[groups.Count-1];
+
+				if (startingIndex < lastGroup.StartingIndex)
+					{  throw new Exception("You must add groups to GroupedTopics in order.");  }
+				if (startingIndex < lastGroup.StartingIndex + lastGroup.Count)
+					{  throw new Exception("Groups in GroupTopics cannot overlap.");  }
+				}
+			#endif
+
+			groups.Add( new TopicGroup(topics, startingIndex, count) );
+			}
+
+
+		/* Function: RemoveGroupAndTopics
+		 * Removes the group from the list and any <Topics> associated with it.
+		 */
+		public void RemoveGroupAndTopics (int groupIndex)
+			{
+			var groupToRemove = groups[groupIndex];
+
+			if (groupToRemove.Count > 0)
+				{  
+				topics.RemoveRange(groupToRemove.StartingIndex, groupToRemove.Count);  
+
+				for (int i = groupIndex + 1; i < groups.Count; i++)
+					{  groups[i].StartingIndex -= groupToRemove.Count;  }
+				}
+
+			groups.RemoveAt(groupIndex);
+			}
+
+
+		/* Function: MergeGroupInto
+		 * Adds the members of a group to the end of a group in another list.  The original group and its topics will be
+		 * deleted.  If the original group has a header it will be discarded.
+		 */
+		public void MergeGroupInto (int groupIndex, GroupedTopics target, int targetGroupIndex)
+			{
+			#if DEBUG
+			if (target == this)
+				{  throw new Exception("Cannot use MergeGroupInto() to transfer members in the same list.");  }
+			#endif
+
+			var group = groups[groupIndex];
+
+			int topicIndex_WithHeader = group.StartingIndex;
+			int topicIndex_NoHeader = group.StartingIndex;
+			int topicCount_WithHeader = group.Count;
+			int topicCount_NoHeader = group.Count;
+
+			if (group.Header != null)
+				{
+				topicIndex_NoHeader++;
+				topicCount_NoHeader--;
+				}
+
+			var targetGroup = target.groups[targetGroupIndex];
+			int targetTopicIndex = targetGroup.StartingIndex + targetGroup.Count;
+
+
+			// Move the topics
+
+			target.topics.InsertRange(targetTopicIndex, topics.GetRange(topicIndex_NoHeader, topicCount_NoHeader));
+			topics.RemoveRange(topicIndex_WithHeader, topicCount_WithHeader);
+
+
+			// Adjust the group counts and indexes
+
+			groups.RemoveAt(groupIndex);
+
+			for (int i = groupIndex; i < groups.Count; i++)
+				{  groups[i].StartingIndex -= topicCount_WithHeader;  }
+
+			targetGroup.Count += topicCount_NoHeader;
+			targetGroup.MembersChanged();
+
+			for (int i = targetGroupIndex + 1; i < target.groups.Count; i++)
+				{  target.groups[i].StartingIndex += topicCount_NoHeader;  }
+			}
+
+
+		/* Function: MoveGroupTo
+		 * Moves the group and its topics to the target list.  If a target group index is specified it will be inserted
+		 * at that position.  If not it will be added to the end of the list.
+		 */
+		public void MoveGroupTo (int groupIndex, GroupedTopics target, int targetGroupIndex = -1)
+			{
+			#if DEBUG
+			if (target == this)
+				{  throw new Exception("Cannot use MoveGroupTo() to transfer members in the same list.");  }
+			#endif
+
+			if (targetGroupIndex == -1)
+				{  targetGroupIndex = target.groups.Count;  }
+
+			var group = groups[groupIndex];
+			int topicIndex = group.StartingIndex;
+			int topicCount = group.Count;
+
+			int targetTopicIndex;
+			if (targetGroupIndex >= target.groups.Count)
+				{  
+				var lastGroup = target.groups[ target.groups.Count - 1 ];
+				targetTopicIndex = lastGroup.StartingIndex + lastGroup.Count;
+				}
+			else
+				{  targetTopicIndex = target.groups[targetGroupIndex].StartingIndex;  }
+
+
+			// Move the topics
+
+			target.topics.InsertRange(targetTopicIndex, topics.GetRange(topicIndex, topicCount));
+			topics.RemoveRange(topicIndex, topicCount);
+
+
+			// Adjust the indexes of groups after the insertion.
+
+			for (int i = groupIndex + 1; i < groups.Count; i++)
+				{  groups[i].StartingIndex -= topicCount;  }
+
+			for (int i = targetGroupIndex; i < target.groups.Count; i++)
+				{  target.groups[i].StartingIndex += topicCount;  }
+
+
+			// Move the group entry
+
+			groups.RemoveAt(groupIndex);
+
+			group.ParentList = target.topics;
+			group.StartingIndex = targetTopicIndex;
+
+			target.groups.Insert(targetGroupIndex, group);
+			}
+
+
+		/* Function: AppendToGroup
+		 * Adds a <Topic> to the end of the group.
+		 */
+		public void AppendToGroup (int groupIndex, Topic topic)
+			{
+			var group = groups[groupIndex];
+
+			topics.Insert(group.StartingIndex + group.Count, topic);
+			group.Count++;
+			group.MembersChanged();
+
+			for (int i = groupIndex + 1; i < groups.Count; i++)
+				{  groups[i].StartingIndex++;  }
+			}
+
+
+
+		// Group: Properties
+		// __________________________________________________________________________
+
+
+		/* Property: Topics
+		 * The <Topics> in the list.
+		 */
+		public List<Topic> Topics
+			{
+			get
+				{  return topics;  }
+			}
+
+
+		/* Property: Groups
+		 * The <TopicGroups> in the list.
+		 */
+		public List<TopicGroup> Groups
+			{
+			get
+				{  return groups;  }
+			}
+
+
+
+		// Group: Variables
+		// __________________________________________________________________________
+
+
+		/* var: topics
+		 */
+		protected List<Topic> topics;
+
+		/* var: groups
+		 */
+		protected List<TopicGroup> groups;
+
+		}
+	}
---
+++ Engine/Source/Topics/TopicGroup.cs
@@ -0,0 +1,374 @@
+﻿/* 
+ * Class: GregValure.NaturalDocs.Engine.Topics.TopicGroup
+ * ____________________________________________________________________________
+ * 
+ * A class to store information about a group of consecutive <Topics> in a list.
+ * 
+ * Topic: Restrictions
+ * 
+ *		- The group can start with a group topic, but it's not required.  However, it must not contain any 
+ *		   group topics beyond that.
+ *		- All topics must be in the same class.
+ *		- It cannot contain topics that start a scope.
+ */
+
+// This file is part of Natural Docs, which is Copyright © 2003-2012 Greg Valure.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+using System.Collections.Generic;
+using GregValure.NaturalDocs.Engine.Collections;
+using GregValure.NaturalDocs.Engine.TopicTypes;
+
+
+namespace GregValure.NaturalDocs.Engine.Topics
+	{
+	public class TopicGroup
+		{
+
+		// Group: Functions
+		// __________________________________________________________________________
+
+
+		/* Constructor: TopicGroup
+		 * Creates a new topic group from the passed section of the list.  Make sure all topics in the group follow
+		 * the <Restrictions>.
+		 */
+		public TopicGroup (List<Topic> topics, int startingIndex, int count)
+			{
+			this.topics = topics;
+			this.startingIndex = startingIndex;
+			this.count = count;
+
+			if (topics[startingIndex].TopicTypeID == Engine.Instance.TopicTypes.IDFromKeyword("group"))
+				{  this.header = topics[startingIndex];  }
+
+			this.recalculateProperties = true;
+
+			Validate();
+			}
+
+
+		/* Function: MembersChanged
+		 * Call whenever you change the members of the group.
+		 */
+		public void MembersChanged ()
+			{
+			recalculateProperties = true;
+			}
+
+
+
+		// Group: Protected Functions
+		// __________________________________________________________________________
+
+
+		/* Function: Validate
+		 * Makes sure all the members follow the <Restrictions>.  Throws an exception if not.
+		 */
+		protected void Validate ()
+			{
+			#if DEBUG
+
+			if (count == 0)
+				{  return;  }
+		
+			var groupTopicTypeID = Engine.Instance.TopicTypes.IDFromKeyword("group");
+			var classString = topics[startingIndex].ClassString;
+
+			for (int i = startingIndex; i < startingIndex + count; i++)
+				{
+				if (topics[i].ClassString != classString)
+					{  throw new Exception("All topics in a TopicGroup must be part of the same class.");  }
+				if (topics[i].TopicTypeID == groupTopicTypeID && i != startingIndex)
+					{  throw new Exception("Only the first topic in a TopicGroup may be a group topic.");  }
+				if (Engine.Instance.TopicTypes.FromID(topics[i].TopicTypeID).Scope == TopicType.ScopeValue.Start)
+					{  throw new Exception("TopicGroups cannot contain topics that start a scope.");  }
+				}
+
+			#endif
+			}
+		
+
+		/* Function: CalculateProperties
+		 * Recalculates <DominantTypeID>, <MixedTypes>, and <TitleMatchesType>.
+		 */
+		protected void CalculateProperties ()
+			{
+			// Calculate dominantTypeID and mixedTypes
+
+			int i = startingIndex;
+
+			if (header != null)
+				{  i++;  }
+
+			if (i >= startingIndex + count)
+				{
+				dominantTypeID = 0;
+				mixedTypes = false;
+				}
+			else
+				{
+				int enumTypeID = Engine.Instance.TopicTypes.IDFromKeyword("enum");
+
+				// See if we can work our way through the group with only one topic type.
+
+				int typeID = topics[i].TopicTypeID;
+				int typeCount = 1;
+				i++;
+
+				while (i < startingIndex + count)
+					{
+					if (topics[i].TopicTypeID == typeID)
+						{
+						i++;
+						typeCount++;
+						}
+					// We allow enum constants through without counting as a different type
+					else if (typeID == enumTypeID && topics[i].IsEmbedded)
+						{
+						i++;
+						}
+					else
+						{  break;  }
+					}
+
+				if (i >= startingIndex + count)
+					{
+					dominantTypeID = typeID;
+					mixedTypes = false;
+					}
+				else
+					{			
+					// Oh well.  Gather the counts for each type.  At least we avoided creating and using a dictionary for
+					// the groups that don't need it.
+
+					SafeDictionary<int, int> typeCounts = new SafeDictionary<int,int>();
+					typeCounts[typeID] = typeCount;
+
+					bool lastNonEmbeddedWasEnum = (typeID == enumTypeID);
+
+					while (i < startingIndex + count)
+						{
+						Topic topic = topics[i];
+
+						if (topic.IsEmbedded == false)
+							{  lastNonEmbeddedWasEnum = (topic.TopicTypeID == enumTypeID);  }
+
+						// Only count embedded topics towards the dominant one if they're not enum constants.
+						if (topic.IsEmbedded == false || lastNonEmbeddedWasEnum == false)
+							{
+							if (typeCounts.ContainsKey(topic.TopicTypeID))
+								{  typeCounts[topic.TopicTypeID]++;  }
+							else
+								{  typeCounts[topic.TopicTypeID] = 1;  }
+							}
+
+						i++;
+						}
+
+					int highestCount = 0;
+
+					foreach (KeyValuePair<int,int> pair in typeCounts)
+						{
+						if (pair.Value > highestCount)
+							{
+							dominantTypeID = pair.Key;
+							highestCount = pair.Value;
+							}
+						}
+
+					mixedTypes = true;
+					}
+				}
+
+
+			// Calculate titleMatchesType
+
+			if (header == null || dominantTypeID == 0)
+				{  titleMatchesType = false;  }
+			else
+				{
+				var type = Engine.Instance.TopicTypes.FromID(dominantTypeID);
+
+				titleMatchesType = (header.Title == type.Name ||
+													 header.Title == type.DisplayName ||
+													 header.Title == type.PluralDisplayName);
+				}
+
+			recalculateProperties = false;
+			}
+
+
+
+		// Group: Properties
+		// __________________________________________________________________________
+
+
+		/* Property: Header
+		 * The group topic, if one exists.  Null otherwise.
+		 */
+		public Topic Header
+			{
+			get
+				{  return header;  }
+			}
+
+		/* Property: Title
+		 * The title of the group, if one exists.  Null otherwise.
+		 */
+		public string Title
+			{
+			get
+				{
+				if (header == null)
+					{  return null;  }
+				else
+					{  return header.Title;  }
+				}
+			}
+
+		/* Property: StartingIndex
+		 * The index of the first member of the group.  If you change this in a way that changes which topics are
+		 * part of the group (as opposed to changing it to reflect shifting indexes due to changes in the topic list)
+		 * you must also call <MembersChanged()>.
+		 */
+		public int StartingIndex
+			{
+			get
+				{  return startingIndex;  }
+			internal set
+				{  startingIndex = value;  }
+			}
+
+		/* Property: Count
+		 * The number of members in the group.  If you change this, you must also call <MembersChanged()>.
+		 */
+		public int Count
+			{
+			get
+				{  return count;  }
+			internal set
+				{  count = value;  }
+			}
+
+		/* Property: IsEmpty
+		 * Whether the group has any topics other than the header.
+		 */
+		public bool IsEmpty
+			{
+			get
+				{
+				if (header == null)
+					{  return (count == 0);  }
+				else
+					{  return (count == 1);  }
+				}
+			}
+
+		/* Property: MixedTypes
+		 * Whether this group has more than one topic type ID.
+		 */
+		public bool MixedTypes
+			{
+			get
+				{  
+				if (recalculateProperties)
+					{  CalculateProperties();  }
+
+				return mixedTypes;  
+				}
+			}
+
+		/* Property: DominantTypeID
+		 * If <MixedTypes> is true, this is the topic type ID that appears the most.  If it's false, this
+		 * is the topic type ID of all the members.
+		 */
+		public int DominantTypeID
+			{
+			get
+				{  
+				if (recalculateProperties)
+					{  CalculateProperties();  }
+
+				return dominantTypeID;  
+				}
+			}
+
+		/* Property: TitleMatchesType
+		 * Whether the group's <Title> matches the name of the <DominantTypeID>.
+		 */
+		public bool TitleMatchesType
+			{
+			get
+				{  
+				if (recalculateProperties)
+					{  CalculateProperties();  }
+
+				return titleMatchesType;  
+				}
+			}
+
+
+		/* Property: ParentList
+		 * The list of <Topics> the group appears in.
+		 */
+		public List<Topic> ParentList
+			{
+			get
+				{  return topics;  }
+			internal set
+				{  topics = value;  }
+			}
+
+
+
+		// Group: Variables
+		// __________________________________________________________________________
+
+		/* var: topics
+		 * The list of topics the group appears in, but the entire list is not the group.
+		 */
+		protected List<Topic> topics;
+
+		/* var: header
+		 * The group header, or null if this is a group without one.
+		 */
+		protected Topic header;
+
+		/* var: startingIndex
+		 * The index of the first <Topic> in the group.  If <header> is defined, it will be the index of that.
+		 */
+		protected int startingIndex;
+
+		/* var: count
+		 * The number of <Topics> in the group.
+		 */
+		protected int count;
+
+		/* var: mixedTypes
+		 * Whether the group has more than one topic type in it.
+		 */
+		protected bool mixedTypes;
+
+		/* var: dominantTypeID
+		 * The topic type ID of the dominant type in the group.  If <mixedTypes> is false, all of the topics
+		 * will be of this type.
+		 */
+		protected int dominantTypeID;
+
+		/* var: titleMatchesType
+		 * Whether the group title matches the name of the dominant topic type ID.
+		 */
+		protected bool titleMatchesType;
+
+		/* var: recalculateProperties
+		 * If true, it means <mixedTypes>, <dominantTypeID>, and <titleMatchesType> need to be recalculated.
+		 */
+		protected bool recalculateProperties;
+
+		}
+	}
+
