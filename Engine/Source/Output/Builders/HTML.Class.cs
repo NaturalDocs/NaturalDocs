6b2cee7d340ee2bbc0960c6399a102b76cf14a9c
Greg Valure <gregvalure@naturaldocs.org>
2012-10-19 11:34:29 -0400

    When a class is defined in multiple files, merge the topics into one cohesive list.

--- Engine/Source/Output/Builders/HTML.Class.cs
+++ Engine/Source/Output/Builders/HTML.Class.cs
@@ -12,9 +12,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using GregValure.NaturalDocs.Engine.Collections;
 using GregValure.NaturalDocs.Engine.Languages;
 using GregValure.NaturalDocs.Engine.Links;
 using GregValure.NaturalDocs.Engine.Symbols;
+using GregValure.NaturalDocs.Engine.Topics;
+using GregValure.NaturalDocs.Engine.TopicTypes;
 
 
 namespace GregValure.NaturalDocs.Engine.Output.Builders
@@ -43,7 +46,7 @@ namespace GregValure.NaturalDocs.Engine.Output.Builders
 			try
 				{
 				ClassString classString = accessor.GetClassByID(classID);
-				IList<Topic> topics = accessor.GetTopicsInClass(classID, cancelDelegate);
+				List<Topic> topics = accessor.GetTopicsInClass(classID, cancelDelegate);
 				
 				if (cancelDelegate())
 					{  return;  }
@@ -131,13 +134,9 @@ namespace GregValure.NaturalDocs.Engine.Output.Builders
 				   haveDBLock = false;
 
 
-					// xxx resort list of topics, since the file order is random
-					// xxx also pick out the title topic
-					// xxx also condense duplicates, like for partial classes and header/source files
-					// xxx may need groups to separate where the files join
-
-
 				   // Build the HTML for the list of topics
+
+					MergeClassTopics(topics);
 
 				   StringBuilder html = new StringBuilder("\r\n\r\n");
 				   HTMLTopic topicBuilder = new HTMLTopic(this);
@@ -237,6 +236,342 @@ namespace GregValure.NaturalDocs.Engine.Output.Builders
 				if (haveDBLock)
 					{  accessor.ReleaseLock();  }
 				}
+			}
+
+
+		/* Function: MergeClassTopics
+		 * Takes a list of <Topics> that come from the same class but multiple source files and combines them into a
+		 * single coherent list.  It assumes all topics from a single file will be consecutive, but otherwise the groups of
+		 * topics can be in any order.
+		 */
+		protected void MergeClassTopics (List<Topic> topics)
+			{
+			if (topics.Count == 0)
+				{  return;  }
+
+			#if DEBUG
+			// Validate that they're all from the same class and that all of a file's topics are consecutive.
+			ClassString classString = topics[0].ClassString;
+			int currentFileID = topics[0].FileID;
+			IDObjects.NumberSet previousFileIDs = new IDObjects.NumberSet();
+
+			for (int i = 1; i < topics.Count; i++)
+				{
+				if (topics[i].ClassString != classString)
+					{  throw new Exception("All topics passed to MergeClassTopics() must have the same class string.");  }
+
+				if (topics[i].FileID != currentFileID)
+					{
+					if (previousFileIDs.Contains(topics[i].FileID))
+						{  throw new Exception("MergeClassTopics() requires all topics that share a file ID be consecutive.");  }
+
+					previousFileIDs.Add(currentFileID);
+					currentFileID = topics[i].FileID;
+					}
+				}
+			#endif
+
+			// If the first and last topic have the same file ID, that means the entire list does and we can return it as is.
+			if (topics[0].FileID == topics[topics.Count-1].FileID)
+				{  return;  }
+
+			var files = Engine.Instance.Files;
+			var topicTypes = Engine.Instance.TopicTypes;
+
+
+			// First we have to sort the topic list by file name.  This ensures that the merge occurs consistently no matter
+			// what order the files in the list are in or how the file IDs were assigned.
+
+			List<Topic> sortedTopics = new List<Topic>(topics.Count);
+
+			do
+				{
+				var lowestFile = files.FromID(topics[0].FileID);
+				var lowestFileIndex = 0;
+				var lastCheckedID = lowestFile.ID;
+
+				for (int i = 1; i < topics.Count; i++)
+					{
+					if (topics[i].FileID != lastCheckedID)
+						{
+						var file = files.FromID(topics[i].FileID);
+
+						if (Path.Compare(file.FileName, lowestFile.FileName) < 0)
+							{  
+							lowestFile = file;  
+							lowestFileIndex = i;
+							}
+
+						lastCheckedID = file.ID;
+						}
+					}
+
+				int count = 0;
+				for (int i = lowestFileIndex; i < topics.Count && topics[i].FileID == lowestFile.ID; i++)
+					{  count++;  }
+
+				sortedTopics.AddRange( topics.GetRange(lowestFileIndex, count) );
+				topics.RemoveRange(lowestFileIndex, count);
+				}
+			while (topics.Count > 0);
+
+
+			// The topics are all in sortedTopics now, and "topics" is empty.  For clarity going forward, let's rename sortedTopics
+			// to remainingTopics, since we have to move them back into topics now.
+
+			List<Topic> remainingTopics = sortedTopics;
+			sortedTopics = null;  // for safety
+
+		
+			// Find the best topic to serve as the class topic.
+
+			int bestClassIndex = 0;
+			long bestClassScore = 0;
+
+			for (int i = 0; i < remainingTopics.Count; i++)
+				{
+				Topic topic = remainingTopics[i];
+
+				if (Instance.TopicTypes.FromID(topic.TopicTypeID).Scope == TopicType.ScopeValue.Start)
+					{
+					long score = CodeDB.Manager.ScoreTopic(topic);
+
+					if (score > bestClassScore)
+						{
+						bestClassIndex = i;
+						bestClassScore = score;
+						}
+					}
+				}
+
+
+			// Copy the best topic in and everything that follows it in the file.  That will serve as the base for merging.
+
+			int bestClassFileID = remainingTopics[bestClassIndex].FileID;
+			int bestClassTopicCount = 1;
+
+			for (int i = bestClassIndex + 1; i < remainingTopics.Count && remainingTopics[i].FileID == bestClassFileID; i++)
+				{  bestClassTopicCount++;  }
+
+			topics.AddRange( remainingTopics.GetRange(bestClassIndex, bestClassTopicCount) );
+			remainingTopics.RemoveRange(bestClassIndex, bestClassTopicCount);
+
+
+			// Delete all the other topics that define the class.  We don't need them anymore.
+
+			int j = 0;
+			while (j < remainingTopics.Count)
+				{
+				if (topicTypes.FromID(remainingTopics[j].TopicTypeID).Scope == TopicType.ScopeValue.Start)
+					{  remainingTopics.RemoveAt(j);  }
+				else
+					{  j++;  }
+				}
+
+
+			// Generate groups from the topic lists.
+
+			var groupedTopics = GetTopicGroups(topics, 1);  // Start at 1 to skip the class topic
+			var groupedRemainingTopics = GetTopicGroups(remainingTopics);
+
+
+			// Delete any empty groups.  We do this on the main group list too for consistency.
+
+			j = 0;
+			while (j < groupedTopics.Groups.Count)
+				{
+				if (groupedTopics.Groups[j].IsEmpty)
+					{  groupedTopics.RemoveGroupAndTopics(j);  }
+				else
+					{  j++;  }
+				}
+
+			j = 0;
+			while (j < groupedRemainingTopics.Groups.Count)
+				{
+				if (groupedRemainingTopics.Groups[j].IsEmpty)
+					{  groupedRemainingTopics.RemoveGroupAndTopics(j);  }
+				else
+					{  j++;  }
+				}
+
+
+			// Now merge groups.  If any remaining groups match the title of an existing group, move its members to
+			// the end of the existing group.
+
+			int remainingGroupIndex = 0;
+			while (remainingGroupIndex < groupedRemainingTopics.Groups.Count)
+				{
+				var remainingGroup = groupedRemainingTopics.Groups[remainingGroupIndex];
+				bool merged = false;
+
+				if (remainingGroup.Title != null)
+					{
+					for (int groupIndex = 0; groupIndex < groupedTopics.Groups.Count; groupIndex++)
+						{
+						if (groupedTopics.Groups[groupIndex].Title == remainingGroup.Title)
+							{
+							groupedRemainingTopics.MergeGroupInto(remainingGroupIndex, groupedTopics, groupIndex);
+							merged = true;
+							break;
+							}
+						}
+					}
+
+				if (merged == false)
+					{  remainingGroupIndex++;  }
+				}
+
+
+			// Move any groups with titles that didn't match to the other list.  We insert it after the last group
+			// of the same dominant type so function groups stay with other function groups, variable groups stay
+			// with other variable groups, etc.
+
+			remainingGroupIndex = 0;
+			while (remainingGroupIndex < groupedRemainingTopics.Groups.Count)
+				{
+				var remainingGroup = groupedRemainingTopics.Groups[remainingGroupIndex];
+
+				if (remainingGroup.Title != null)
+					{
+					int bestMatchIndex = -1;
+
+					// Walk the list backwards because we want it to be after the last group of the type, not the first.
+					for (int i = groupedTopics.Groups.Count - 1; i >= 0; i--)
+						{
+						if (groupedTopics.Groups[i].DominantTypeID == remainingGroup.DominantTypeID)
+							{
+							bestMatchIndex = i;
+							break;
+							}
+						}
+
+					if (bestMatchIndex == -1)
+						{  
+						// Just add them to the end if nothing matches.
+						groupedRemainingTopics.MoveGroupTo(remainingGroupIndex, groupedTopics);  
+						}
+					else
+						{  groupedRemainingTopics.MoveGroupTo(remainingGroupIndex, groupedTopics, bestMatchIndex + 1);  }
+					}
+				else
+					{  remainingGroupIndex++;  }
+				}
+
+
+			// Now we're left with topics that are not in groups.  See if the list contains any titled groups at all.
+
+			bool groupsWithTitles = false;
+
+			foreach (var group in groupedTopics.Groups)
+				{
+				if (group.Title != null)
+					{
+					groupsWithTitles = true;
+					break;
+					}
+				}
+
+
+			// If there's no titles we can just append the remaining topics as is.
+
+			if (groupsWithTitles == false)
+				{
+				groupedTopics.Topics.AddRange(groupedRemainingTopics.Topics);
+				}
+
+
+			// If there are titled groups, see if we can add them to the end of existing groups.  However, only do
+			// this if TitleMatchesType is set.  It's okay to put random functions into the group "Functions" but
+			// not into something more specific.
+
+			else
+				{
+				// We don't care about the remaining groups anymore so we can just work directly on the topics.
+				remainingTopics = groupedRemainingTopics.Topics;
+				groupedRemainingTopics = null;  // for safety
+
+				while (remainingTopics.Count > 0)
+					{
+					int type = remainingTopics[0].TopicTypeID;
+					int matchingGroupIndex = -1;
+
+					for (int i = groupedTopics.Groups.Count - 1; i >= 0; i--)
+						{
+						if (groupedTopics.Groups[i].DominantTypeID == type && 
+							 groupedTopics.Groups[i].TitleMatchesType)
+							{  
+							matchingGroupIndex = i;
+							break;
+							}
+						}
+
+					// Create a new group if there's no existing one we can use.
+					if (matchingGroupIndex == -1)
+						{
+						Topic generatedTopic = new Topic();
+						generatedTopic.TopicID = 0;
+						generatedTopic.Title = Engine.Instance.TopicTypes.FromID(type).PluralDisplayName;
+						generatedTopic.Symbol = SymbolString.FromPlainText_ParenthesesAlreadyRemoved(generatedTopic.Title);
+						generatedTopic.ClassString = topics[0].ClassString;
+						generatedTopic.ClassID = topics[0].ClassID;
+						generatedTopic.TopicTypeID = Engine.Instance.TopicTypes.IDFromKeyword("group");
+						generatedTopic.FileID = topics[0].FileID;
+						generatedTopic.LanguageID = topics[0].LanguageID;
+
+						groupedTopics.Topics.Add(generatedTopic);
+						groupedTopics.CreateGroup(groupedTopics.Topics.Count - 1, 1);
+
+						matchingGroupIndex = groupedTopics.Groups.Count - 1;
+						}
+
+					j = 0;
+					while (j < remainingTopics.Count)
+						{
+						var remainingTopic = remainingTopics[j];
+						if (remainingTopic.TopicTypeID == type)
+							{
+							groupedTopics.AppendToGroup(matchingGroupIndex, remainingTopic);
+							remainingTopics.RemoveAt(j);
+							}
+						else
+							{  j++;  }
+						}
+					}
+				}
+
+			// xxx also condense duplicates, like for partial classes and header/source files
+			}
+
+
+		/* Function: GetTopicGroups
+		 * Returns a list of <TopicGroups> for the passed <Topics>.
+		 */
+		protected GroupedTopics GetTopicGroups (List<Topic> topics, int startingIndex = 0)
+			{
+			GroupedTopics groupedTopics = new GroupedTopics(topics);
+
+			int groupTopicTypeID = Engine.Instance.TopicTypes.IDFromKeyword("group");
+
+			int i = startingIndex;
+			while (i < topics.Count)
+				{
+				int fileID = topics[i].FileID;
+				int groupStart = i;
+				int groupCount = 1;
+
+				i++;
+
+				while (i < topics.Count && topics[i].FileID == fileID && topics[i].TopicTypeID != groupTopicTypeID)
+					{
+					groupCount++;
+					i++;
+					}
+
+				groupedTopics.CreateGroup(groupStart, groupCount);
+				}
+
+			return groupedTopics;
 			}
 
 
