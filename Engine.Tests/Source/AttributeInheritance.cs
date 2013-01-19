b476a943f4fc418a9b0c952937bce84f88fefaed
Greg Valure <gregvalure@naturaldocs.org>
2013-01-19 17:34:27 -0500

    Add tests for language, tag, and access level inheritance.

---
+++ Engine.Tests/Source/AttributeInheritance.cs
@@ -0,0 +1,76 @@
+﻿/* 
+ * Class: GregValure.NaturalDocs.Engine.Tests.AttributeInheritance
+ * ____________________________________________________________________________
+ * 
+ * File-based tests to make sure Natural Docs topics inherit certain attributes correctly.
+ * 
+ */
+
+// This file is part of Natural Docs, which is Copyright © 2003-2012 Greg Valure.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using GregValure.NaturalDocs.Engine;
+using GregValure.NaturalDocs.Engine.Tests.Framework;
+using GregValure.NaturalDocs.Engine.Topics;
+
+
+namespace GregValure.NaturalDocs.Engine.Tests
+	{
+	[TestFixture]
+	public class AttributeInheritance : Framework.SourceToTopics
+		{
+
+		[Test]
+		public void All ()
+			{
+			TestFolder("Attribute Inheritance", "Shared ND Config/Basic Language Support");
+			}
+
+		public override string OutputOf (IList<Topic> topics)
+			{
+			if (topics == null || topics.Count == 0)
+				{  return "(No topics found)";  }
+
+			StringBuilder output = new StringBuilder();
+
+			for (int i = 0; i < topics.Count; i++)
+				{
+				if (i != 0)
+					{  output.AppendLine("-----");  }
+
+				output.AppendLine(topics[i].Title);
+
+				output.AppendLine("- Language: " + Engine.Instance.Languages.FromID(topics[i].LanguageID).Name);
+				output.AppendLine("- Declared Access Level: " + topics[i].DeclaredAccessLevel);
+				output.AppendLine("- Effective Access Level: " + topics[i].EffectiveAccessLevel);
+
+				if (topics[i].TagIDs != null && topics[i].TagIDs.IsEmpty == false)
+					{  
+					output.Append("- Tags: ");
+					bool isFirst = true;
+
+					foreach (int tagID in topics[i].TagIDs)
+						{
+						if (isFirst)
+							{  isFirst = false;  }
+						else
+							{  output.Append(", ");  }
+
+						output.Append(Engine.Instance.TopicTypes.TagFromID(tagID).Name);
+						}
+
+					output.AppendLine();
+					}
+				}
+
+			return output.ToString();
+			}
+
+		}
+	}
