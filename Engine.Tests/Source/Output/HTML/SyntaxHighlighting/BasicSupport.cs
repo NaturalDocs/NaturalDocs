35f9e4a78b25f7227737cc258c0f4d21f45d2413
Greg Valure <gregvalure@naturaldocs.org>
2013-02-13 13:27:41 -0500

    Add syntax highlighting tests for basic language support.

---
+++ Engine.Tests/Source/Output/HTML/SyntaxHighlighting/BasicSupport.cs
@@ -0,0 +1,25 @@
+﻿
+// This file is part of Natural Docs, which is Copyright © 2003-2012 Greg Valure.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+using NUnit.Framework;
+using GregValure.NaturalDocs.Engine.Tests.Framework;
+
+
+namespace GregValure.NaturalDocs.Engine.Tests.Output.HTML.SyntaxHighlighting
+	{
+	[TestFixture]
+	public class BasicSupport : Framework.SourceToHTML
+		{
+
+		[Test]
+		public void All ()
+			{
+			TestFolder("Output/HTML/Syntax Highlighting/Basic Support", "Shared ND Config/Basic Language Support", "pre");
+			}
+
+		}
+	}
