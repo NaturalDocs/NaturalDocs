016a8aea3be71d982aa7942ee3e9ac4ee1e9d14e
Greg Valure <gregvalure@naturaldocs.org>
2013-02-20 17:51:47 -0500

    Remove wayward tab causing "ref" to not be highlighted in C#.

--- Engine/Source/Languages/Parsers/CSharp.cs
+++ Engine/Source/Languages/Parsers/CSharp.cs
@@ -2139,7 +2139,7 @@ namespace GregValure.NaturalDocs.Engine.Languages.Parsers
 			"internal", "is", "lock", "long", "namespace",
 			"new", "null", "object", "operator", "out",
 			"override", "params", "private", "protected", "public",
-			"readonly", "ref	", "return", "sbyte", "sealed",
+			"readonly", "ref", "return", "sbyte", "sealed",
 			"short", "sizeof", "stackalloc", "static", "string",
 			"struct", "switch", "this", "throw", "true",
 			"try", "typeof", "uint", "ulong", "unchecked",
