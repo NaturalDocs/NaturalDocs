ce6eac8ae611321718ef99ab6fc97403b84c9ae3
Greg Valure <gregvalure@naturaldocs.org>
2013-03-07 21:19:51 -0500

    Tests for removing "partial" from class prototypes.

---
+++ Engine.Tests.Data/Output/HTML/Class Prototypes/C#/Partial - Input.cs
@@ -0,0 +1,12 @@
+
+partial class PartialClass { }
+
+partial struct PartialStruct { }
+
+partial interface PartialInterface { }
+
+public static partial class EndOfModifiers { }
+
+partial public static class BeginningOfModifiers { }
+
+public partial static class MiddleOfModifiers { }
