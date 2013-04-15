c9bb778fd1d57de7750a7192a4ee8b60c9e13115
Greg Valure <gregvalure@naturaldocs.org>
2013-04-15 09:56:31 -0400

    Tests for improved entity char handling in XML.

---
+++ Engine.Tests.Data/Comments/XML/Parsing/Entity Chars - Input.cs
@@ -0,0 +1,12 @@
+
+/// <summary>This & that.  This &amp; that.  This &#38; that.  This &#x26; that.</summary>
+void Test1 () { }
+
+/// <summary>Copyright © Me.  Copyright &copy; Me.  Copyright &#169; Me.  Copyright &#xA9; Me.</summary>
+void Test2 () { }
+
+/// <summary><code>" & < ></code></summary>
+void Test3 () { }
+
+/// <summary><code>&quot; &#34; &#x22;</code></summary>
+void Test4 () { }
