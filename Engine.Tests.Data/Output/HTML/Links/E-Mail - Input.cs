5fa1b5a8a225adb398139d869a43a05f73990779
Greg Valure <gregvalure@naturaldocs.org>
2013-04-30 13:37:23 -0400

    Add tests for HTML link generation.

---
+++ Engine.Tests.Data/Output/HTML/Links/E-Mail - Input.cs
@@ -0,0 +1,26 @@
+﻿
+/* Function: Natural Docs bracketed
+	Link to <email@address.com>.
+*/
+
+/* Function: Natural Docs bare
+	Link to email@address.com
+*/
+
+/* Function: Natural Docs named
+	Link to <somebody at email@address.com>
+*/
+
+/**
+ * Link to <a href="mailto:email@address.com">somebody</a>.
+ * @return x
+ */
+void JavadocHTML () { }
+
+/**
+ * Link to <a href="mailto:email@address.com?Subject=Something">somebody</a>.
+ * @return x
+ */
+void JavadocHTMLWithSubject () { }
+
+
---
+++ Engine.Tests.Data/Output/HTML/Links/URLs - Input.cs
@@ -0,0 +1,20 @@
+﻿
+/* Function: Natural Docs bracketed
+	Link to <http://www.website.com>.
+*/
+
+/* Function: Natural Docs bare
+	Link to http://www.website.com
+*/
+
+/* Function: Natural Docs named
+	Link to <web site at http://www.website.com>
+*/
+
+/**
+ * Link to <a href="http://www.website.com">web site</a>.
+ * @return x
+ */
+void JavadocHTML () { }
+
+
