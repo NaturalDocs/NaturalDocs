6b8ba2649ecf7a8575c195ec03acf51eab2aa6a6
Greg Valure <gregvalure@naturaldocs.org>
2013-08-16 19:39:03 -0400

    Add an implementation of array.indexOf() so IE 8 and earlier work.

--- Engine/Resources/Styles/DefaultJS/NDCore.js
+++ Engine/Resources/Styles/DefaultJS/NDCore.js
@@ -795,3 +795,47 @@ function NDLocation (hashString)
 	this.Constructor(hashString);
 
 	};
+
+
+// IE 8 and earlier don't have array.indexOf().  Add it. This code matches the standard algorithm according to 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf)
+	{
+	Array.prototype.indexOf = function (searchElement /*, fromIndex */ )
+		{
+		"use strict";
+
+		if (this == null)
+			{  throw new TypeError();  }
+
+		var n, k, t = Object(this),
+		len = t.length >>> 0;
+
+		if (len === 0)
+			{  return -1;  }
+
+		n = 0;
+
+		if (arguments.length > 1)
+			{
+			n = Number(arguments[1]);
+			if (n != n)  // shortcut for verifying if it's NaN
+				{  n = 0;  }
+			else if (n != 0 && n != Infinity && n != -Infinity)
+				{
+				n = (n > 0 || -1) * Math.floor(Math.abs(n));
+				}
+			}
+	
+		if (n >= len)
+			{  return -1;  }
+
+		for (k = n >= 0 ? n : Math.max(len - Math.abs(n), 0); k < len; k++)
+			{
+			if (k in t && t[k] === searchElement)
+				{  return k;  }
+			}
+
+		return -1;
+		};
+	}
