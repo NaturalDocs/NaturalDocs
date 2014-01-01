5fbdf87d4c22754436f9c8b0c25f1dfb5b40251e
Greg Valure <gregvalure@naturaldocs.org>
2014-01-01 14:42:13 -0500

    Add link interpretation tests.

---
+++ Engine.Tests/Source/Framework/TestTypes/LinkInterpretations.cs
@@ -0,0 +1,78 @@
+﻿/* 
+ * Class: GregValure.NaturalDocs.Engine.Tests.Framework.TestTypes.LinkInterpretations
+ * ____________________________________________________________________________
+ * 
+ * A class to test Natural Docs' link interpreting.
+ * 
+ * Commands:
+ * 
+ *		> // text
+ *		Comment.
+ *		
+ *		> <Link text>
+ *		The link to show the interpretations of.
+ */
+
+// This file is part of Natural Docs, which is Copyright © 2003-2013 Greg Valure.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using GregValure.NaturalDocs.Engine;
+using GregValure.NaturalDocs.Engine.Links;
+using GregValure.NaturalDocs.Engine.Tests.Framework;
+
+
+namespace GregValure.NaturalDocs.Engine.Tests.Framework.TestTypes
+	{
+	public class LinkInterpretations : Framework.TextCommands
+		{
+
+		public override string OutputOf (IList<string> commands)
+			{
+			StringBuilder output = new StringBuilder();
+			var parser = Engine.Instance.Comments.NaturalDocsParser;
+
+			foreach (var command in commands)
+				{
+				output.AppendLine(command);
+
+				if (command.StartsWith("<"))
+					{
+					try
+						{
+						string link = command.Trim();
+						string parameters = null;
+
+						var interpretations = parser.LinkInterpretations(link,
+																					   Engine.Comments.Parsers.NaturalDocs.LinkInterpretationFlags.AllowNamedLinks |
+																					   Engine.Comments.Parsers.NaturalDocs.LinkInterpretationFlags.AllowPluralsAndPossessives |
+																					   Engine.Comments.Parsers.NaturalDocs.LinkInterpretationFlags.FromOriginalText,
+																					   out parameters);
+
+						if (parameters != null)
+							{  output.AppendLine("- Parameters -> " + parameters);  }
+
+						foreach (var interpretation in interpretations)
+							{  output.AppendLine("- \"" + interpretation.Text + "\" -> " + interpretation.Target);  }
+						}
+					catch (Exception e)
+						{
+						output.AppendLine("ERROR: Exception " + e.Message);
+						}
+					}
+				else if (command != null && command.Trim() != "" && command.StartsWith("//") == false)
+					{
+					output.AppendLine("ERROR: Unrecognized command");
+					}
+				}
+
+			return output.ToString();
+			}
+
+		}
+	}
