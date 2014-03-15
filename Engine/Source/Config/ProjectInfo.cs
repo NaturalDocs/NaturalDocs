91905c6f64a4bd98b198e3b3f91c0e2d616b1a7b
Greg Valure <gregvalure@naturaldocs.org>
2014-03-15 19:32:52 -0400

    Make static functions for timestamp generation.

--- Engine/Source/Config/ProjectInfo.cs
+++ Engine/Source/Config/ProjectInfo.cs
@@ -53,16 +53,39 @@ namespace GregValure.NaturalDocs.Engine.Config
 			}
 			
 
-		/* Property: MakeTimeStamp
+		/* Functoin: MakeTimeStamp
 		 * Generates a time stamp from <TimeStampCode> and the current date.  If <TimeStampCode> is null this will also
 		 * return null.
 		 */
 		public string MakeTimeStamp ()
 			{
+			return MakeTimeStamp(timeStampCode);
+			}
+
+
+
+		// Group: Static Functions
+		// __________________________________________________________________________
+
+
+		/* Function: MakeTimeStamp
+		 */
+		public static string MakeTimeStamp (string timeStampCode)
+			{
+			return MakeTimeStamp (timeStampCode, DateTime.Now.ToLocalTime());
+			}
+
+
+		/* Function: MakeTimeStamp
+		 */
+		public static string MakeTimeStamp (string timeStampCode, DateTime date)
+			{
 			if (timeStampCode == null)
 				{  return null;  }
 
-			DateTime now = DateTime.Now.ToLocalTime();
+			if (date == null)
+				{  date = DateTime.Now.ToLocalTime();  }
+
 			Tokenization.Tokenizer tokenizer = new Tokenization.Tokenizer(timeStampCode);
 			Tokenization.TokenIterator tokenIterator = tokenizer.FirstToken;
 			System.Text.StringBuilder output = new System.Text.StringBuilder();
@@ -70,47 +93,47 @@ namespace GregValure.NaturalDocs.Engine.Config
 			while (tokenIterator.IsInBounds)
 				{
 				if (tokenIterator.MatchesToken("m", true))
-					{  output.Append(now.Month);  }
+					{  output.Append(date.Month);  }
 				else if (tokenIterator.MatchesToken("mm", true))
 					{
-					if (now.Month < 10)
+					if (date.Month < 10)
 						{  output.Append('0');  }
-					output.Append(now.Month);
+					output.Append(date.Month);
 					}
 				else if (tokenIterator.MatchesToken("mon", true))
-					{  output.Append( Locale.Get("NaturalDocs.Engine", "TimeStamp.ShortMonth" + now.Month) );  }
+					{  output.Append( Locale.Get("NaturalDocs.Engine", "TimeStamp.ShortMonth" + date.Month) );  }
 				else if (tokenIterator.MatchesToken("month", true))
-					{  output.Append( Locale.Get("NaturalDocs.Engine", "TimeStamp.Month" + now.Month) );  }
+					{  output.Append( Locale.Get("NaturalDocs.Engine", "TimeStamp.Month" + date.Month) );  }
 				else if (tokenIterator.MatchesToken("d", true))
-					{  output.Append(now.Day);  }
+					{  output.Append(date.Day);  }
 				else if (tokenIterator.MatchesToken("dd", true))
 					{
-					if (now.Day < 10)
+					if (date.Day < 10)
 						{  output.Append('0');  }
-					output.Append(now.Day);
+					output.Append(date.Day);
 					}
 				else if (tokenIterator.MatchesToken("day", true))
 					{
-					output.Append(now.Day);
-					if (now.Day == 1 || now.Day == 21 || now.Day == 31)
+					output.Append(date.Day);
+					if (date.Day == 1 || date.Day == 21 || date.Day == 31)
 						{  output.Append("st");  }
-					else if (now.Day == 2 || now.Day == 22)
+					else if (date.Day == 2 || date.Day == 22)
 						{  output.Append("nd");  }
-					else if (now.Day == 3 || now.Day == 23)
+					else if (date.Day == 3 || date.Day == 23)
 						{  output.Append("rd");  }
 					else
 						{  output.Append("th");  }
 					}
 				else if (tokenIterator.MatchesToken("yy", true))
 					{
-					int year = now.Year % 100;
+					int year = date.Year % 100;
 					if (year < 10)
 						{  output.Append('0');  }
 					output.Append(year);
 					}
 				else if (tokenIterator.MatchesToken("yyyy", true) || tokenIterator.MatchesToken("year", true))
 					{
-					output.Append(now.Year);
+					output.Append(date.Year);
 					}
 				else
 					{
