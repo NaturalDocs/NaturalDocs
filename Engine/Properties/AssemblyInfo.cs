73dce43670cd6bf73849565f40bfa00f753e20f0
Greg Valure <gregvalure@naturaldocs.org>
2020-03-03 11:03:40 -0500

    Update version to 2.1 (Development Release 1)

--- Engine/Properties/AssemblyInfo.cs
+++ Engine/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.2.90")]
-[assembly: AssemblyFileVersion("2.0.2.90")]
+[assembly: AssemblyVersion("2.1.0.11")]
+[assembly: AssemblyFileVersion("2.1.0.11")]
--- Engine/Source/Instance.cs
+++ Engine/Source/Instance.cs
@@ -536,7 +536,7 @@ namespace CodeClear.NaturalDocs.Engine
 		/* Constant: VersionString
 		 * The current version of the Natural Docs engine as a string.
 		 */
-		public const string VersionString = "2.0.2";
+		public const string VersionString = "2.1 (Development Release 1)";
 		
 		
 		/* Property: Version
