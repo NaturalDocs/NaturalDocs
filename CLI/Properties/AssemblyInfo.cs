43e3a53069e0d37308ba68dbcd476fc3362e397f
Greg Valure <gregvalure@naturaldocs.org>
2020-04-11 20:38:40 -0400

    Update version to 2.1 (Development Release 2)

--- CLI/Properties/AssemblyInfo.cs
+++ CLI/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.0.11")]
-[assembly: AssemblyFileVersion("2.1.0.11")]
+[assembly: AssemblyVersion("2.1.0.12")]
+[assembly: AssemblyFileVersion("2.1.0.12")]
