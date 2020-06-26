db2af5dacfc33929bf3ec7fc6c6e8d56cea1e13e
Greg Valure <gregvalure@naturaldocs.org>
2020-06-26 12:54:02 -0400

    Add test for 3-way class merging.

---
+++ Engine.Tests.Data/General/Class Combining/3WayMerging - Input A.cs
@@ -0,0 +1,14 @@
+
+/* Class: 3WayMerging
+	Description to make this the first file.
+*/
+public partial class 3WayMerging
+	{
+
+    // Group: Group From File A
+
+    // Function: FunctionA
+
+    // Function: FunctionB
+
+	}
---
+++ Engine.Tests.Data/General/Class Combining/3WayMerging - Input B.cs
@@ -0,0 +1,17 @@
+
+/* Class: 3WayMerging
+*/
+public partial class 3WayMerging
+	{
+
+    // Group: Group From Files B and C
+    // These need to combine even though they didn't appear in file A.
+
+    // Function: FunctionC
+    // Longer description from file B
+
+    // Function: FunctionD
+
+    // Function: FunctionE1
+
+	}
