895306a65f7e2a5ac8b9290af82ee08dbf013ef7
Greg Valure <gregvalure@naturaldocs.org>
2020-09-29 14:03:21 -0400

    Change version to 2.1 (Beta 1).

--- Engine/Source/Instance.cs
+++ Engine/Source/Instance.cs
@@ -601,7 +601,7 @@ namespace CodeClear.NaturalDocs.Engine
 		/* Constant: VersionString
 		 * The current version of the Natural Docs engine as a string.
 		 */
-		public const string VersionString = "2.1 (Development Release 2)";
+		public const string VersionString = "2.1 (Beta 1)";
 
 				
 
