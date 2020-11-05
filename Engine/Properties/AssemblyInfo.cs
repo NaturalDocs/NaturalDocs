076fc75fdde0cab751acf6ee46fdea42a8cf2021
Greg Valure <gregvalure@naturaldocs.org>
2020-11-05 18:51:15 -0500

    Change version to 2.2 (Development Release 1).

--- Engine/Properties/AssemblyInfo.cs
+++ Engine/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.0.62")]
+[assembly: AssemblyVersion("2.2.0.11")]
 [assembly: AssemblyFileVersion("2.1.0.62")]
