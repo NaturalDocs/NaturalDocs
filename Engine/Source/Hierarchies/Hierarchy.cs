a1cfb4241ca08795b7f0d8940416590c6fefc216
Greg Valure <gregvalure@naturaldocs.org>
2021-05-01 10:18:43 -0400

    Add existing hierarchies to Hierarchies.Manager.

---
+++ Engine/Source/Hierarchies/Hierarchy.cs
@@ -0,0 +1,99 @@
+﻿/* 
+ * Class: CodeClear.NaturalDocs.Engine.Hierarchies.Hierarchy
+ * ____________________________________________________________________________
+ * 
+ * Information about a single hierarchy within Natural Docs.
+ * 
+ */
+
+// This file is part of Natural Docs, which is Copyright © 2003-2021 Code Clear LLC.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+
+
+namespace CodeClear.NaturalDocs.Engine.Hierarchies
+	{
+	public class Hierarchy
+		{
+		
+		// Group: Functions
+		// __________________________________________________________________________
+		
+		
+		/* Constructor: Hierarchy
+		 */
+		public Hierarchy (string name, string pluralName, HierarchyType type)
+			{
+			this.name = name;
+			this.pluralName = pluralName;
+			this.type = type;
+			}
+
+
+
+		// Group: Properties
+		// __________________________________________________________________________
+
+
+		/* Property: ID
+		 * The numeric ID of the hierarchy.
+		 */
+		public int ID
+			{
+			get
+				{  return (int)type;  }
+			}
+
+		/* Property: Name
+		 * The name of the hierarchy.
+		 */
+		public string Name
+			{
+			get
+				{  return name;  }
+			}
+
+		/* Property: PluralName
+		 * The plural name of the hierarchy, or null if none.
+		 */
+		public string PluralName
+			{
+			get
+				{  return pluralName;  }
+			}
+
+		/* Property: Type
+		 * The <HierarchyType> of the hierarchy.
+		 */
+		public HierarchyType Type
+			{
+			get
+				{  return type;  }
+			}
+
+
+
+		// Group: Variables
+		// __________________________________________________________________________
+
+
+		/* var: name
+		 * The name of the hierarchy.
+		 */
+		protected string name;
+
+		/* var: pluralName
+		 * The plural name of the hierarchy, or null if none.
+		 */
+		protected string pluralName;
+
+		/* var: type
+		 * The <HierarchyType> associated with this hierarchy.
+		 */
+		protected HierarchyType type;
+
+		}
+	}
--- Engine/Source/Hierarchies/Manager.cs
+++ Engine/Source/Hierarchies/Manager.cs
@@ -17,6 +17,7 @@
 
 
 using System;
+using System.Collections.Generic;
 
 
 namespace CodeClear.NaturalDocs.Engine.Hierarchies
@@ -32,6 +33,15 @@ namespace CodeClear.NaturalDocs.Engine.Hierarchies
 		 */
 		public Manager (Engine.Instance engineInstance) : base (engineInstance)
 			{
+			var fileHierarchy = new Hierarchy("File", "Files", HierarchyType.File);
+
+			var classHierarchy = new Hierarchy("Class", "Classes", HierarchyType.Class);
+
+			var databaseHierarchy = new Hierarchy("Database", null, HierarchyType.Database);
+
+			hierarchies = new Hierarchy[] { fileHierarchy, classHierarchy, databaseHierarchy };
+
+			classHierarchyID = classHierarchy.ID;
 			}
 
 		protected override void Dispose (bool strictRulesApply)
@@ -50,6 +60,102 @@ namespace CodeClear.NaturalDocs.Engine.Hierarchies
 			{
 			return true;
 			}
+
+
+
+		// Group: Information Functions
+		// __________________________________________________________________________
+
+
+		/* Function: FromID
+		 * Returns the <Hierarchy> associated with the passed ID, or null if none.
+		 */
+		public Hierarchy FromID (int hierarchyID)
+			{
+			foreach (var hierarchy in hierarchies)
+				{
+				if (hierarchy.ID == hierarchyID)
+					{  return hierarchy;  }
+				}
+
+			return null;
+			}
+
+		/* Function: FromName
+		 * Returns the <Hierarchy> associated with the passed name, or null if none.  This will search both the singular
+		 * and plural names.
+		 */
+		public Hierarchy FromName (string name)
+			{
+			Collections.KeySettings normalizationSettings = Collections.KeySettings.IgnoreCase;
+
+			string normalizedName = name.NormalizeKey(normalizationSettings);
+
+			foreach (var hierarchy in hierarchies)
+				{
+				if (hierarchy.Name.NormalizeKey(normalizationSettings) == normalizedName)
+					{  return hierarchy;  }
+				if (hierarchy.PluralName != null &&
+					hierarchy.PluralName.NormalizeKey(normalizationSettings) == normalizedName)
+					{  return hierarchy;  }
+				}
+
+			return null;
+			}
+
+		/* Function: FromType
+		 * Returns the <Hierarchy> associated with the passed <HierarchyType>.
+		 */
+		public Hierarchy FromType (HierarchyType hierarchyType)
+			{
+			foreach (var hierarchy in hierarchies)
+				{
+				if (hierarchy.Type == hierarchyType)
+					{  return hierarchy;  }
+				}
+
+			return null;
+			}
+
+
+
+		// Group: Convenience Functions
+		// __________________________________________________________________________
+
+
+		/* Function: IsClassHierarchy
+		 * Returns whether the passed ID is the class hierarchy ID.  A convenience function since this is used so often.
+		 */
+		public bool IsClassHierarchy (int hierarchyID)
+			{
+			// We're assuming the class hierarchy is always defined, which should be pretty safe, so we only have to test
+			// against classHierarchyID and not also test against zero.  However, if the class hierarchy can ever be undefined
+			// then this logic needs to be updated or else it will return true for every non-hierarchy element.
+
+			#if DEBUG
+			if (classHierarchyID == 0)
+				{  throw new Exception("IsClassHierarchy needs to be updated if classHierarchyID can be zero.");  }
+			#endif
+
+			return (hierarchyID == classHierarchyID);
+			}
+
+
+
+		// Group: Variables
+		// __________________________________________________________________________
+
+
+		/* var: hierarchies
+		 * An array of the <Hierarchies> defined.  Since it should be a short, hard-coded list for now it's more efficient to
+		 * just put them in an array.
+		 */
+		protected Hierarchy[] hierarchies;
+
+		/* var: classHierarchyID
+		 * A copy of the class hierarchy ID since it is used so often.  Prevents some lookups in <hierarchies>.
+		 */
+		protected int classHierarchyID;
 
 		}
 	}
