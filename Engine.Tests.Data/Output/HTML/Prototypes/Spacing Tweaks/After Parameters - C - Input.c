78779b061bec0824ecfc3c74ec9fe8c22a1293d8
Greg Valure <gregvalure@naturaldocs.org>
2022-09-25 12:07:23 -0400

    Add tests for new closing parentheses alignment behavior in prototypes.

---
+++ Engine.Tests.Data/Output/HTML/Prototypes/Spacing Tweaks/After Parameters - C - Input.c
@@ -0,0 +1,46 @@
+
+// Function: FunctionA
+void FunctionA (int a, int b)
+  {
+  }
+
+// Function: FunctionB
+void FunctionB (int aa, int b)
+  {
+  }
+
+// Function: FunctionC
+void FunctionC (int aaa, int b)
+  {
+  }
+
+// Function: FunctionD
+void FunctionD (int a, int bb)
+  {
+  }
+
+// Function: FunctionE
+void FunctionE (int a, int bbb)
+  {
+  }
+
+// Function: FunctionF
+void FunctionF (int a = 12, int b)
+  {
+  }
+
+// Function: FunctionG
+void FunctionG (int a, int b = 12)
+  {
+  }
+
+// Function: FunctionH
+void FunctionH (int a = 12, int b) some content after closing
+  {
+  }
+
+// Function: FunctionI
+void FunctionI (int a, int b = 12) some content after closing
+  {
+  }
+
---
+++ Engine.Tests.Data/Output/HTML/Prototypes/Spacing Tweaks/After Parameters - C# - Input.cs
@@ -0,0 +1,27 @@
+
+// Property: PropertyA
+int PropertyA
+	{
+	get
+		{ }
+	set
+		{ }
+	}
+
+// Property: PropertyB
+int PropertyB
+	{
+	init
+		{ }
+	set
+		{ }
+	}
+
+// Property: PropertyC
+int PropertyC
+	{
+	get
+		{ }
+	init
+		{ }
+	}
