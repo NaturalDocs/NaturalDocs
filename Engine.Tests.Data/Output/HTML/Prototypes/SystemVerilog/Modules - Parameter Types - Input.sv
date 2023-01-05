17359c868aedfa086c70ec2d0c91324abe71f75a
Greg Valure <gregvalure@naturaldocs.org>
2023-01-05 11:57:18 -0500

    Add SystemVerilog prototype tests for the inline parameter types supported so far.

---
+++ Engine.Tests.Data/Output/HTML/Prototypes/SystemVerilog/Modules - Parameter Types - Input.sv
@@ -0,0 +1,21 @@
+
+// Group: Data Types
+
+// Module: IntegerVectorTypes
+module IntegerVectorTypes (logic a, reg [7:0] b, bit unsigned [12:0] c, bit signed [10:0][] d);
+
+// Module: IntegerAtomTypes
+module IntegerAtomTypes (byte a, shortint signed b, int c, longint unsigned d, integer e, time f);
+
+// Module: NonIntegerTypes
+module NonIntegerTypes (shortreal a, real b, realtime c);
+
+// Module: EnumTypes
+module EnumTypes (
+	enum { a1, a2[1], a3[2:3], a4 = 3, a5[2] = 1 } a,
+	enum byte { b1 } b,
+	enum int signed { c1 = 0 }[4:0] c, 
+	enum bit unsigned [7:0] { d1 }[][] d);
+
+// Module: OtherTypes
+module OtherTypes (string a, chandle b, event c);
