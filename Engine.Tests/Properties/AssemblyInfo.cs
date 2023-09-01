ac39a752bed8cb70eb859de8bf1d8826412362e2
Greg Valure <gregvalure@naturaldocs.org>
2023-09-01 17:03:25 -0400

    Change version number to 2.3.

--- Engine.Tests/Properties/AssemblyInfo.cs
+++ Engine.Tests/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.3.0.81")]
-[assembly: AssemblyFileVersion("2.3.0.81")]
+[assembly: AssemblyVersion("2.3.0.90")]
+[assembly: AssemblyFileVersion("2.3.0.90")]
