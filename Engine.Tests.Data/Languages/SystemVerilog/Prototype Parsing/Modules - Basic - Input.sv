107a881b003de57268eed95539bd545ed50000f3
Greg Valure <gregvalure@naturaldocs.org>
2024-09-03 16:44:53 -0400

    Add basic module parsing tests for SystemVerilog.

---
+++ Engine.Tests.Data/Languages/SystemVerilog/Prototype Parsing/Modules - Basic - Input.sv
@@ -0,0 +1,28 @@
+
+// Module: ModuleKeyword
+module ModuleKeyword ();
+endmodule
+
+// Module: MacroModuleKeyword
+macromodule MacroModuleKeyword ();
+endmodule
+
+// Module: StaticLifetime
+module static StaticLifetime ();
+endmodule
+
+// Module: AutomaticLifetime
+module automatic AutomaticLifetime ();
+endmodule
+
+// Module: Complex_Identifier$
+module Complex_Identifier$ ();
+endmodule
+
+// Module: NamedEnd
+module NamedEnd ();
+endmodule: NamedEnd
+
+// Module: NamedEnd_Complex_Identifier$
+module NamedEnd_Complex_Identifier$ ();
+endmodule: NamedEnd_Complex_Identifier$
