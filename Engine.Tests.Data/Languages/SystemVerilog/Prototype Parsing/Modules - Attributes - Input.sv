3703155776548cf326662ad2c0b562112b54b749
Greg Valure <gregvalure@naturaldocs.org>
2024-09-03 16:45:47 -0400

    Update SystemVerilog attribute and import module test files to be syntactically valid.

--- Engine.Tests.Data/Languages/SystemVerilog/Prototype Parsing/Modules - Attributes - Input.sv
+++ Engine.Tests.Data/Languages/SystemVerilog/Prototype Parsing/Modules - Attributes - Input.sv
@@ -2,20 +2,24 @@
 // Module: NoValue
 (* optimize_power *)
 module NoValue ();
+endmodule
 
 // Module: WithValue
 (* optimize_power=1 *)
 module WithValue ();
+endmodule
 
 // Module: WithExpression
 (* expr_value = (12 + 9) % 2 *)
 module WithExpression ();
+endmodule
 
 // Module: MultipleAttributeBlock
 (* optimize_power = 0, mode = "cla",
     no_value, expr_value = (12 + 9) % 2,
 	cond_value = x ? 1 : 0 *)
 module MultipleAttributeBlock ();
+endmodule
 
 // Module: MultipleAttributeStatements
 (* optimize_power = 0 *)
@@ -24,3 +28,4 @@ module MultipleAttributeBlock ();
 (* expr_value = (12 + 9) % 2 *)
 (* cond_value = x ? 1 : 0 *)
 module MultipleAttributeStatements ();
+endmodule
--- Engine.Tests.Data/Languages/SystemVerilog/Prototype Parsing/Modules - Import - Input.sv
+++ Engine.Tests.Data/Languages/SystemVerilog/Prototype Parsing/Modules - Import - Input.sv
@@ -3,25 +3,30 @@
 module ImportPackage
 	import PackageName::*;
 	();
+endmodule
 
 // Module: ImportMember
 module ImportMember
 	import PackageName::MemberName;
 	();
+endmodule
 
 // Module: ImportMultiple1
 module ImportMultiple1
 	import PackageA::*, PackageB::MemberName;
 	();
+endmodule
 
 // Module: ImportMultiple2
 module ImportMultiple2
 	import PackageA::*;
 	import PackageB::MemberName;
 	();
+endmodule
 
 // Module: ImportMultiple3
 module ImportMultiple3
 	import PackageA::*, PackageB::MemberName;
 	import PackageC::*;
 	();
+endmodule
