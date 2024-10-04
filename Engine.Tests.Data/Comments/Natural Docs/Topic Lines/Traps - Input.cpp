f1b9f482ffd84a497b01856b161e445655fd5bef
Greg Valure <gregvalure@naturaldocs.org>
2024-10-04 16:46:21 -0400

    Add tests for some topic line traps.

---
+++ Engine.Tests.Data/Comments/Natural Docs/Topic Lines/Traps - Input.cpp
@@ -0,0 +1,52 @@
+
+// Topic: .LeadingDot
+
+// Topic: ..LeadingDoubleDot
+
+// Topic: Double..MiddleDot
+
+// Topic: EndingDot.
+
+// Topic: EndingDoubleDot..
+
+// Topic: .
+
+// Topic: ..
+
+// Function: .LeadingDotFunction
+void LeadingDotFunction ();
+
+// Function: ..LeadingDoubleDotFunction
+void LeadingDoubleDotFunction ();
+
+// Function: EndingDotFunction.
+void EndingDotFunction ();
+
+// Function: EndingDoubleDotFunction..
+void EndingDoubleDotFunction ();
+
+// Topic: ::LeadingColons
+
+// Topic: ::::LeadingDoubleColons
+
+// Topic: Double::::MiddleColons
+
+// Topic: EndingColons::
+
+// Topic: EndingDoubleColons::::
+
+// Topic: ::
+
+// Topic: ::::
+
+// Function: ::LeadingColonsFunction
+void LeadingColonsFunction ();
+
+// Function: ::::LeadingDoubleColonsFunction
+void LeadingDoubleColonsFunction ();
+
+// Function: EndingColonsFunction::
+void EndingColonsFunction ();
+
+// Function: EndingDoubleColonsFunction::::
+void EndingDoubleColonsFunction ();
