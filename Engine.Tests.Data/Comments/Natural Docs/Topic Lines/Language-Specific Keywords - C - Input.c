40326998aa65889d1b0fb136a46565e160a32e01
Greg Valure <gregvalure@naturaldocs.org>
2024-10-07 15:02:03 -0400

    Add language-specific keyword tests.

---
+++ Engine.Tests.Data/Comments/Natural Docs/Topic Lines/Language-Specific Keywords - C - Input.c
@@ -0,0 +1,47 @@
+
+// Section: Records
+// ______________________________________________
+
+// Record: MyRecordA
+// Should be a class in C#, undefined in other languages
+
+// C# Record: MyRecordB
+// Should always be a class
+
+
+// Section: Tables
+// ______________________________________________
+
+// Table: MyTableA
+// Should be a database table in SQL, variable in other languages
+
+// DB Table: MyTableB
+// Should always be a database table
+
+// DBTable: MyTableC
+// Should always be a database table
+
+// Database Table: MyTableD
+// Should always be a database table
+
+// DatabaseTable: MyTableE
+// Should always be a database table
+
+// SQL Table: MyTableF
+// Should always be a database table
+
+// PL/SQL Table: MyTableG
+// Should always be a database table
+
+
+// Section: Fields
+// ______________________________________________
+
+// Field: MyFieldA
+// Should be a database field in SQL only, variable in other languages
+
+// DB Field: MyFieldB
+// Should always be a database field
+
+// SQL Field: MyFieldC
+// Should always be a database field
