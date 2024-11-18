aa2c7ba2ee16f4bb42390c7f66330c192c25be78
Greg Valure <gregvalure@naturaldocs.org>
2024-11-18 18:20:19 -0500

    Add tests for modules with various combinations of port and parameter port lists.

--- Engine.Tests.Data/Languages/SystemVerilog/Prototype Parsing/Modules - Basic - Input.sv
+++ Engine.Tests.Data/Languages/SystemVerilog/Prototype Parsing/Modules - Basic - Input.sv
@@ -26,3 +26,43 @@ endmodule: NamedEnd
 // Module: NamedEnd_Complex_Identifier$
 module NamedEnd_Complex_Identifier$ ();
 endmodule: NamedEnd_Complex_Identifier$
+
+
+// Group: Port Lists
+// ______________________________________________
+
+// Module: PortListOnlyA
+module PortListOnlyA ();
+endmodule
+
+// Module: PortListOnlyB
+module PortListOnlyB (logic portA);
+endmodule
+
+// Module: ParameterPortListOnlyA
+module ParameterPortListOnlyA #();
+endmodule
+
+// Module: ParameterPortListOnlyB
+module ParameterPortListOnlyB #(parameter paramA = 1);
+endmodule
+
+// Module: PortAndParameterPortListsA
+module PortAndParameterPortListsA #() ();
+endmodule
+
+// Module: PortAndParameterPortListsB
+module PortAndParameterPortListsB #(parameter paramA = 1) (logic portA);
+endmodule
+
+// Module: PortAndParameterPortListsC
+module PortAndParameterPortListsC #() (logic portA);
+endmodule
+
+// Module: PortAndParameterPortListsD
+module PortAndParameterPortListsD #(parameter paramA = 1) ();
+endmodule
+
+// Module: NoPortLists
+module NoPortLists;
+endmodule
