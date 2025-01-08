d7629980d7df453e866d7b7ad5af5014ec62ff4d
Greg Valure <gregvalure@naturaldocs.org>
2025-01-07 23:54:52 -0500

    Add "endmodule" to tests to make them syntactically valid.

--- Engine.Tests.Data/Output/HTML/Prototypes/SystemVerilog/Modules - Attributes and Extern - Input.sv
+++ Engine.Tests.Data/Output/HTML/Prototypes/SystemVerilog/Modules - Attributes and Extern - Input.sv
@@ -4,16 +4,19 @@
 // Module: NoValue
 (* optimize_power *)
 module NoValue ();
+endmodule
 
 // Module: WithValue
 (* optimize_power=1 *)
 module WithValue ();
+endmodule
 
 // Module: ManyValues
 (* optimize_power = 0, mode = "cla",
     no_value, expr_value = (12 + 9) % 2,
 	cond_value = x ? 1 : 0 *)
 module ManyValues ();
+endmodule
 
 // Module: SeparateValues
 (* optimize_power = 0 *)
@@ -22,9 +25,11 @@ module ManyValues ();
 (* expr_value = (12 + 9) % 2,
 	cond_value = x ? 1 : 0 *)
 module SeparateValues ();
+endmodule
 
 // Module: Spacing
 (*optimize_power=0,mode="cla"*)(*no_value*)module Spacing();
+endmodule
 
 
 // Group: Extern
@@ -34,14 +39,18 @@ module SeparateValues ();
 extern
 (* optimize_power=1 *)
 module ExternAttributeMultipleLines ();
+endmodule
 
 // Module: ExternAttributeSingleLine
 // Should still format the same as ExternAttributeMultipleLines.
 extern (* optimize_power=1 *) module ExternAttributeSingleLine ();
+endmodule
 
 // Module: ExternOnly
 // If there are no attributes "extern" shouldn't be on its own line.
-extern module ExternOnly ();  
+extern module ExternOnly ();
+endmodule
 
 // Module: ExternSpacing
 extern(*optimize_power=0,mode="cla"*)(*no_value*)module ExternSpacing();
+endmodule
