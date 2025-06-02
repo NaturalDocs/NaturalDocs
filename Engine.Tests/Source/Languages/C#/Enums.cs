77e69a12d258dba2ec1faf11b6932da32f6ce0c1
Greg Valure <gregvalure@naturaldocs.org>
2025-06-02 11:55:14 -0400

    Add C# tests for enums.  Vertical star lines still fail at this point.

---
+++ Engine.Tests/Source/Languages/C#/Enums.cs
@@ -0,0 +1,24 @@
+﻿
+// This file is part of Natural Docs, which is Copyright © 2003-2025 Code Clear LLC.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+using NUnit.Framework;
+
+
+namespace CodeClear.NaturalDocs.Engine.Tests.Languages.CSharp
+	{
+	[TestFixture]
+	public class Enums : Framework.TestTypes.Enums
+		{
+
+		[Test]
+		public void All ()
+			{
+			TestFolder("Languages/C#/Enums");
+			}
+
+		}
+	}
