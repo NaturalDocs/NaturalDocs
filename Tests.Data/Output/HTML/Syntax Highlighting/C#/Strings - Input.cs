316f7c9a6e072bb4b52204bae28962e8fa2a25d1
Greg Valure <gregvalure@naturaldocs.org>
2025-08-31 15:55:57 -0400

    Add additional interpolated string syntax highlighting tests for C#.

--- Tests.Data/Output/HTML/Syntax Highlighting/C#/Strings - Input.cs
+++ Tests.Data/Output/HTML/Syntax Highlighting/C#/Strings - Input.cs
@@ -50,17 +50,39 @@
 
         string interpolatedString1 = $"abc {def} ghi";
 
-        string interpolatedString2 = $"abc {obj.Function(12, false)} ghi";
+        string interpolatedString2 = @$"abc {4+5} ghi";
+
+		string interpolatedString3 = $"abc {obj.Function(12, false)} ghi";
+
+        string interpolatedString4 = $@"abc { (x == y ? "y" : "n") } ghi";
+
+		string interpolatedString5 = $"abc {{ def";
+
+        string interpolatedString6 = $"abc {{ def } ghi";
+
+        string interpolatedString7 = $"abc { def }} ghi";
+
+        string interpolatedString8 = $"abc {{ def }} ghi";
 
 		string interpolatedVerbatimString1 = $@"abc {def} ghi";
 
         string interpolatedVerbatimString2 = @$"abc {4+5} ghi";
 
-        string interpolatedVerbatimString3 = $@"abc { (x == y ? "y" : "n") } ghi";
+		string interpolatedVerbatimString3 = $"abc {obj.Function(12, false)} ghi";
 
-        string interpolatedVerbatimString4 = @$"abc {{ def";
+		string interpolatedVerbatimString4 = $@"abc { (x == y ? "y" : "n") } ghi";
 
-		string invalidInterpolatedString = $"abc { def";
+        string interpolatedVerbatimString5 = @$"abc {{ def";
+
+        string interpolatedVerbatimString6 = $@"abc {{ def } ghi";
+
+        string interpolatedVerbatimString7 = @$"abc { def }} ghi";
+
+        string interpolatedVerbatimString8 = $@"abc {{ def }} ghi";
+
+		string invalidInterpolatedString1 = $"abc { def";
+
+		string invalidInterpolatedString2 = @$"abc { def";
 
 		(end)
 
