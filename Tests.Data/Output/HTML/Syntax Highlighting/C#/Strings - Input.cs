d8f316f99896ef1463c6b64561bfadf72b27d5eb
Greg Valure <gregvalure@naturaldocs.org>
2025-08-31 23:46:47 -0400

    Add tests for interpolated raw strings in C# with an arbitrary amount of $ symbols.

--- Tests.Data/Output/HTML/Syntax Highlighting/C#/Strings - Input.cs
+++ Tests.Data/Output/HTML/Syntax Highlighting/C#/Strings - Input.cs
@@ -126,27 +126,155 @@
 
 	Topic: Interpolated Raw Strings
 
+		If there is a single $, then braces are interpolations.  Unlike quoted and verbatim strings, you cannot use double braces to
+		escape them.
+
 		--- code
 
-		string interpolatedRawString1 = $"""abc {def} ghi""";
+		string interpolatedRawString1_1_1 = $"""abc {def} ghi""";
 
-		string interpolatedRawString2 = $""""abc {4+5} ghi"""";
+		string interpolatedRawString1_1_2 = $""""abc {4+5} ghi"""";
 
-		string interpolatedRawString3 = $"""""abc {obj.Function(12, false)} ghi""""";
+		string interpolatedRawString1_1_3 = $"""""abc {obj.Function(12, false)} ghi""""";
 
-		string interpolatedRawString4 = $""""""abc { (x == y ? "y" : "n") } ghi"""""";
+		string interpolatedRawString1_1_4 = $""""""abc { (x == y ? "y" : "n") } ghi"""""";
 
-		string interpolatedRawString5 = $"""abc {{ def""";
+		string interpolatedRawString1_1_5 = $"""abc { def }} ghi""";
 
-		string interpolatedRawString6 = $""""abc {{ def } ghi"""";
+		---
 
-		string interpolatedRawString7 = $"""""abc { def }} ghi""""";
+		If there is a double $, single braces are part of the string, double braces are interpolations, triple braces have one as part of
+		the string and two as the interpolation, and four or more are invalid.
 
-		string interpolatedRawString8 = $""""""abc {{ def }} ghi"""""";
+		--- code
 
-		string interpolatedRawString9 = $"""""""abc {{{ def }}} ghi""""""";
+		string interpolatedRawString2_1_1 = $$"""abc {def} ghi""";
 
-		string interpolatedRawString10 = $""""""""abc {{{{ def }}}} ghi"""""""";
+		string interpolatedRawString2_1_2 = $$""""abc {4+5} ghi"""";
+
+		string interpolatedRawString2_1_3 = $$"""""abc {obj.Function(12, false)} ghi""""";
+
+		string interpolatedRawString2_1_4 = $$""""""abc { (x == y ? "y" : "n") } ghi"""""";
+
+		string interpolatedRawString2_1_5 = $$"""abc {def}} ghi""";
+
+		string interpolatedRawString2_2_1 = $$"""abc {{def}} ghi""";
+
+		string interpolatedRawString2_2_2 = $$""""abc {{4+5}} ghi"""";
+
+		string interpolatedRawString2_2_3 = $$"""""abc {{obj.Function(12, false)}} ghi""""";
+
+		string interpolatedRawString2_2_4 = $$""""""abc {{ (x == y ? "y" : "n") }} ghi"""""";
+
+		string interpolatedRawString2_2_5 = $$"""abc {{def}}} ghi""";
+
+		string interpolatedRawString2_3_1 = $$"""abc {{{def}}} ghi""";
+
+		string interpolatedRawString2_3_2 = $$""""abc {{{4+5}}} ghi"""";
+
+		string interpolatedRawString2_3_3 = $$"""""abc {{{obj.Function(12, false)}}} ghi""""";
+
+		string interpolatedRawString2_3_4 = $$""""""abc {{{ (x == y ? "y" : "n") }}} ghi"""""";
+
+		string interpolatedRawString2_3_5 = $$"""abc {{{def}}}}} ghi""";
+
+		---
+
+		If there is a triple $, single and double braces are part of the string, triple braces are interpolations, four and five have one
+		and two as part of the string and then the three following as the interpolation, and six or more are invalid.
+
+		--- code
+
+		string interpolatedRawString3_1_1 = $$$"""abc {def} ghi""";
+
+		string interpolatedRawString3_1_2 = $$$""""abc {4+5} ghi"""";
+
+		string interpolatedRawString3_1_3 = $$$"""""abc {obj.Function(12, false)} ghi""""";
+
+		string interpolatedRawString3_1_4 = $$$""""""abc { (x == y ? "y" : "n") } ghi"""""";
+
+		string interpolatedRawString3_1_5 = $$$"""abc {def}} ghi""";
+
+		string interpolatedRawString3_2_1 = $$$"""abc {{def}} ghi""";
+
+		string interpolatedRawString3_2_2 = $$$""""abc {{4+5}} ghi"""";
+
+		string interpolatedRawString3_2_3 = $$$"""""abc {{obj.Function(12, false)}} ghi""""";
+
+		string interpolatedRawString3_2_4 = $$$""""""abc {{ (x == y ? "y" : "n") }} ghi"""""";
+
+		string interpolatedRawString3_2_5 = $$$"""abc {{def}}} ghi""";
+
+		string interpolatedRawString3_3_1 = $$$"""abc {{{def}}} ghi""";
+
+		string interpolatedRawString3_3_2 = $$$""""abc {{{4+5}}} ghi"""";
+
+		string interpolatedRawString3_3_3 = $$$"""""abc {{{obj.Function(12, false)}}} ghi""""";
+
+		string interpolatedRawString3_3_4 = $$$""""""abc {{{ (x == y ? "y" : "n") }}} ghi"""""";
+
+		string interpolatedRawString3_3_5 = $$$"""abc {{{def}}}} ghi""";
+
+		string interpolatedRawString3_4_1 = $$$"""abc {{{{def}}}} ghi""";
+
+		string interpolatedRawString3_4_2 = $$$""""abc {{{{4+5}}}} ghi"""";
+
+		string interpolatedRawString3_4_3 = $$$"""""abc {{{{obj.Function(12, false)}}}} ghi""""";
+
+		string interpolatedRawString3_4_4 = $$$""""""abc {{{{{ (x == y ? "y" : "n") }}}} ghi"""""";
+
+		string interpolatedRawString3_4_5 = $$$"""abc {{{{def}}}}}} ghi""";
+
+		string interpolatedRawString3_5_1 = $$$"""abc {{{{{def}}}}} ghi""";
+
+		string interpolatedRawString3_5_2 = $$$""""abc {{{{{4+5}}}}} ghi"""";
+
+		string interpolatedRawString3_5_3 = $$$"""""abc {{{{{obj.Function(12, false)}}}}} ghi""""";
+
+		string interpolatedRawString3_5_4 = $$$""""""abc {{{{{ (x == y ? "y" : "n") }}}}} ghi"""""";
+
+		string interpolatedRawString3_5_5 = $$$"""abc {{{{{def}}}}}}} ghi""";
+
+		---
+
+		This continues for an arbitrary amount of $ symbols, just like there can be an arbitrary number of quotes.
+
+		--- code
+
+		string interpolatedRawString5_3 = $$$$$"""abc {{{def}}} ghi""";
+
+		string interpolatedRawString5_5 = $$$$$"""abc {{{{{def}}}}} ghi""";
+
+		string interpolatedRawString5_7 = $$$$$"""abc {{{{{{{def}}}}}}} ghi""";
+
+		string interpolatedRawString7_4 = $$$$$$$"""abc {{{{def}}}} ghi""";
+
+		string interpolatedRawString7_7 = $$$$$$$"""abc {{{{{{{def}}}}}}} ghi""";
+
+		string interpolatedRawString7_9 = $$$$$$$"""abc {{{{{{{{{def}}}}}}}}} ghi""";
+
+		---
+
+		The below strings are all invalid so how they are handled isn't important, just that the parser doesn't hang
+		or crash.
+
+		--- code
+
+		string invalidRawString1 = $"""abc { def""";
+
+		string invalidRawString2 = $$"""abc {{ def""";
+
+		string invalidRawString3 = $$$"""abc {{{ def""";
+
+		string invalidRawString4 = $"""abc {{ def }} ghi""";
+
+		string invalidRawString5 = $$"""abc {{{{ def }}}} ghi""";
+
+		string invalidRawString6 = $$$"""abc {{{{{{ def }}}}}} ghi""";
+
+		string invalidRawString7 = $"""abc {{ def } ghi""";
+
+		string invalidRawString8 = $$"""abc {{{{ def }} ghi""";
 
 		---
 
@@ -214,4 +342,5 @@
 			"""""";
 
 		---
+
 */
