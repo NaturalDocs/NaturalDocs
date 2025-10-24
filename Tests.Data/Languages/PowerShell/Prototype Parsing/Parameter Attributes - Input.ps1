f1bb2f837ad36d5173b56db141d6366ced423dfe
Greg Valure <gregvalure@naturaldocs.org>
2025-10-24 15:14:49 -0400

    Add PowerShell prototype parsing tests.

---
+++ Tests.Data/Languages/PowerShell/Prototype Parsing/Parameter Attributes - Input.ps1
@@ -0,0 +1,48 @@
+
+# Function: BareParameters
+function BareParameters ($a, $b, $c)
+	{
+	}
+
+# Function: ParameterTypes
+function ParameterTypes ([string] $a,
+										[int] $b,
+										[switch] $c,
+										[DateTime] $d,
+										[PSCredential] $e,
+										[object[]] $f,
+										[System.IO.FileInfo] $g)
+	{
+	}
+
+# Function: ParameterModifiers
+function ParameterModifiers ([Parameter(Mandatory=$true)] $a,
+											[Parameter(Position=2)] $b,
+											[Parameter(Mandatory=$true, Position=3)] $c,
+											[ValidateRange(0,10)] $d,
+											[ValidateNotNull] $e,
+											[Alias("f")] $f,
+											[Parameter(Mandatory=$true, Position=7)][ValidateNotNull][Alias('g',"ggg")][ValidateRange(0,10)] $g)
+	{
+	}
+
+# Function: DetermineTypeA
+function DetermineTypeA ([Parameter(Mandatory=$true)][Alias("a")][ValidateNotNull][string] $a,
+										[string][Parameter(Mandatory=$true)][Alias("a")][ValidateNotNull] $b,
+										[Parameter(Mandatory=$true)][string][Alias("a")][ValidateNotNull] $c)
+	{
+	}
+
+# Function: DetermineTypeB
+function DetermineTypeB ([Parameter(Mandatory=$true)][Alias("b")][ValidateNotNull][DateTime] $a,
+										[DateTime][Parameter(Mandatory=$true)][Alias("b")][ValidateNotNull] $b,
+										[Parameter(Mandatory=$true)][DateTime][Alias("b")][ValidateNotNull] $c)
+	{
+	}
+
+# Function: DetermineTypeC
+function DetermineTypeC ([Parameter(Mandatory=$true)][Alias("c")][ValidateNotNull][System.IO.FileInfo[]] $a,
+										[System.IO.FileInfo[]][Parameter(Mandatory=$true)][Alias("c")][ValidateNotNull] $b,
+										[Parameter(Mandatory=$true)][System.IO.FileInfo[]][Alias("c")][ValidateNotNull] $c)
+	{
+	}
