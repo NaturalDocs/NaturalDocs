361a1857e36077f66ed915d2b5bc941b475f47e6
Greg Valure <gregvalure@naturaldocs.org>
2025-12-07 15:08:09 -0500

    Add documented only class member tests.

---
+++ Tests.Data/Output/HTML/Documented Only/Class Members - C# - Input.cs
@@ -0,0 +1,189 @@
+
+class UndocumentedClass
+	{
+
+	void UndocumentedClass_UndocumentedFunction ()
+		{
+		}
+
+	// Function: UndocumentedClass_NDDocumentedFunction
+	void UndocumentedClass_NDDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * UndocumentedClass_NDHeaderlessDocumentedFunction
+	 */
+	void UndocumentedClass_NDHeaderlessDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * @see UndocumentedClass_JavadocDocumentedFunction
+	 */
+	void UndocumentedClass_JavadocDocumentedFunction ()
+		{
+		}
+
+	/// <summary>
+	/// UndocumentedClass_XMLDocumentedFunction
+	/// </summary>
+	void UndocumentedClass_XMLDocumentedFunction ()
+		{
+		}
+
+	}
+
+
+// Class: NDDocumentedClass
+class NDDocumentedClass
+	{
+
+	void NDDocumentedClass_UnocumentedFunction ()
+		{
+		}
+
+	// Function: NDDocumentedClass_NDDocumentedFunction
+	void NDDocumentedClass_NDDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * NDDocumentedClass_NDHeaderlessDocumentedFunction
+	 */
+	void NDDocumentedClass_NDHeaderlessDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * @see NDDocumentedClass_JavadocDocumentedFunction
+	 */
+	void NDDocumentedClass_JavadocDocumentedFunction ()
+		{
+		}
+
+	/// <summary>
+	/// NDDocumentedClass_XMLDocumentedFunction
+	/// </summary>
+	void NDDocumentedClass_XMLDocumentedFunction ()
+		{
+		}
+
+	}
+
+
+/**
+ * NDHeaderlessDocumentedClass
+ */
+class NDHeaderlessDocumentedClass
+	{
+
+	void NDHeaderlessDocumentedClass_UnocumentedFunction ()
+		{
+		}
+
+	// Function: NDHeaderlessDocumentedClass_NDDocumentedFunction
+	void NDHeaderlessDocumentedClass_NDDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * NDHeaderlessDocumentedClass_NDHeaderlessDocumentedFunction
+	 */
+	void NDHeaderlessDocumentedClass_NDHeaderlessDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * @see NDHeaderlessDocumentedClass_JavadocDocumentedFunction
+	 */
+	void NDHeaderlessDocumentedClass_JavadocDocumentedFunction ()
+		{
+		}
+
+	/// <summary>
+	/// NDHeaderlessDocumentedClass_XMLDocumentedFunction
+	/// </summary>
+	void NDHeaderlessDocumentedClass_XMLDocumentedFunction ()
+		{
+		}
+
+	}
+
+
+/**
+ * @see JavadocDocumentedClass
+ */
+class JavadocDocumentedClass
+	{
+
+	void JavadocDocumentedClass_UnocumentedFunction ()
+		{
+		}
+
+	// Function: JavadocDocumentedClass_NDDocumentedFunction
+	void JavadocDocumentedClass_NDDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * JavadocDocumentedClass_NDHeaderlessDocumentedFunction
+	 */
+	void JavadocDocumentedClass_NDHeaderlessDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * @see JavadocDocumentedClass_JavadocDocumentedFunction
+	 */
+	void JavadocDocumentedClass_JavadocDocumentedFunction ()
+		{
+		}
+
+	/// <summary>
+	/// JavadocDocumentedClass_XMLDocumentedFunction
+	/// </summary>
+	void JavadocDocumentedClass_XMLDocumentedFunction ()
+		{
+		}
+
+	}
+
+
+/// <summary>
+/// XMLDocumentedClass
+/// </summary>
+class XMLDocumentedClass
+	{
+
+	void XMLDocumentedClass_UnocumentedFunction ()
+		{
+		}
+
+	// Function: XMLDocumentedClass_NDDocumentedFunction
+	void XMLDocumentedClass_NDDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * XMLDocumentedClass_NDHeaderlessDocumentedFunction
+	 */
+	void XMLDocumentedClass_NDHeaderlessDocumentedFunction ()
+		{
+		}
+
+	/**
+	 * @see XMLDocumentedClass_JavadocDocumentedFunction
+	 */
+	void XMLDocumentedClass_JavadocDocumentedFunction ()
+		{
+		}
+
+	/// <summary>
+	/// XMLDocumentedClass_XMLDocumentedFunction
+	/// </summary>
+	void XMLDocumentedClass_XMLDocumentedFunction ()
+		{
+		}
+
+	}
