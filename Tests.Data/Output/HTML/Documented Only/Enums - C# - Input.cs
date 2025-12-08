200096006f46e7488b59ed3887a895ee9da2c0d3
Greg Valure <gregvalure@naturaldocs.org>
2025-12-08 12:48:05 -0500

    Add tests for using Documented Only with enums.

---
+++ Tests.Data/Output/HTML/Documented Only/Enums - C# - Input.cs
@@ -0,0 +1,37 @@
+
+enum UndocumentedEnum
+	{
+
+	UndocumentedEnum_UndocumentedValue,
+
+	UndocumentedEnum_QuickDocumentedValue // Quick description
+
+	}
+
+
+/* Enum: DocumentedEnumWithCommentValues
+
+	Values:
+
+		DocumentedEnumWithCommentValues_UndocumentedValue - Comment description
+		DocumentedEnumWithCommentValues_QuickDocumentedValue - Comment description
+*/
+enum DocumentedEnumWithCommentValues
+	{
+
+	DocumentedEnumWithCommentValues_UndocumentedValue,
+
+	DocumentedEnumWithCommentValues_QuickDocumentedValue // Quick description
+
+	}
+
+
+// Enum: DocumentedEnumWithoutCommentValues
+enum DocumentedEnumWithoutCommentValues
+	{
+
+	DocumentedEnumWithoutCommentValues_UndocumentedValue,
+
+	DocumentedEnumWithoutCommentValues_QuickDocumentedValue // Quick description
+
+	}
