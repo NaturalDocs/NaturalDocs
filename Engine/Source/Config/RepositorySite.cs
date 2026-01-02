1300712903f87a0b4235ed2c40c685318b9ae94d
Greg Valure <gregvalure@naturaldocs.org>
2026-01-02 00:01:41 -0500

    Store properties of known repository sites, currently GitHub, GitLab, CodeBerg, and Gitea.

---
+++ Engine/Source/Config/RepositorySite.cs
@@ -0,0 +1,177 @@
+﻿/*
+ * Class: CodeClear.NaturalDocs.Engine.Config.RepositorySite
+ * ____________________________________________________________________________
+ *
+ * A class storing information about a known repository site.
+ *
+ */
+
+// This file is part of Natural Docs, which is Copyright © 2003-2025 Code Clear LLC.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+using System.Text.RegularExpressions;
+
+
+namespace CodeClear.NaturalDocs.Engine.Config
+	{
+	public class RepositorySite
+		{
+
+		// Group: Functions
+		// __________________________________________________________________________
+
+
+		/* Function: RepositorySite
+		 */
+		public RepositorySite (string name, Regex isSiteURLRegex, Regex isProjectURLRegex, string sourceURLTemplate_WithBranch,
+										string sourceURLTemplate_WithoutBranch, string exampleProjectURL)
+			{
+			this.name = name;
+			this.isSiteURLRegex = isSiteURLRegex;
+			this.isProjectURLRegex = isProjectURLRegex;
+			this.sourceURLTemplate_WithBranch = sourceURLTemplate_WithBranch;
+			this.sourceURLTemplate_WithoutBranch = sourceURLTemplate_WithoutBranch;
+			this.exampleProjectURL = exampleProjectURL;
+			}
+
+
+		/* Function: IsSiteURL
+		 * Returns whether the URL is from anywhere on this repository site.
+		 */
+		public bool IsSiteURL (string url)
+			{
+			return isSiteURLRegex.IsMatch(url);
+			}
+
+
+		/* Function: IsProjectURL
+		 * Returns whether the URL is to a project page on this repository site.
+		 */
+		public bool IsProjectURL (string url)
+			{
+			return isProjectURLRegex.IsMatch(url);
+			}
+
+
+		/* Function: SourceURLTemplate
+		 * Creates a source URL template with the passed project URL and optionally the branch name as well.  The resulting
+		 * URL template will only require relative path and line number substitutions.
+		 */
+		public string SourceURLTemplate (string projectURL, string branchName = null)
+			{
+
+			// Determine which template to use
+
+			string sourceURLTemplate;
+
+			if (branchName != null && sourceURLTemplate_WithBranch != null)
+				{  sourceURLTemplate = sourceURLTemplate_WithBranch;  }
+			else
+				{  sourceURLTemplate = sourceURLTemplate_WithoutBranch;  }
+
+			#if DEBUG
+			if (sourceURLTemplate == null)
+				{  throw new Exception ("Couldn't create a source URL template.");  }
+			#endif
+
+
+			// If the project URL ends with / and that's also the first character after the substitution point, remove it from the
+			// project URL.  This allows project URLs like "https://github.com/NaturalDocs/NaturalDocs/" and templates like
+			// "{ProjectURL}/blob/..." to work without creating a double slash.
+
+			if (projectURL[projectURL.Length - 1] == '/')
+				{
+				#if DEBUG
+				if (RepositorySubstitutions.ProjectURL.Length != 0)
+					{  throw new Exception ("Assumed RepositorySubstitutions.ProjectURL was a single character.");  }
+				#endif
+
+				int projectURLSubstitutionIndex = sourceURLTemplate.IndexOf(RepositorySubstitutions.ProjectURL[0]);
+
+				if (projectURLSubstitutionIndex != -1 &&
+					projectURLSubstitutionIndex + 1 < sourceURLTemplate.Length &&
+					sourceURLTemplate[projectURLSubstitutionIndex + 1] == '/')
+					{
+					projectURL = projectURL.Substring(0, projectURL.Length - 1);
+					}
+				}
+
+
+			// Perform the substitutions
+
+			sourceURLTemplate = sourceURLTemplate.Replace(RepositorySubstitutions.ProjectURL, projectURL);
+
+			if (branchName != null)
+				{  sourceURLTemplate = sourceURLTemplate.Replace(RepositorySubstitutions.Branch, branchName);  }
+
+			return sourceURLTemplate;
+			}
+
+
+
+		// Group: Properties
+		// __________________________________________________________________________
+
+
+		/* Property: Name
+		 * The name of the repository site, such as "GitHub".
+		 */
+		public string Name
+			{
+			get
+				{  return name;  }
+			}
+
+
+		/* Property: ExampleProjectURL
+		 * A string to use as an example project URL in error messages, such as "https://github.com/username/projectname/"
+		 */
+		public string ExampleProjectURL
+			{
+			get
+				{  return exampleProjectURL;  }
+			}
+
+
+
+		// Group: Variables
+		// __________________________________________________________________________
+
+
+		/* var: name
+		 * The name of the repository site, such as "GitHub".
+		 */
+		protected string name;
+
+		/* var: isSiteURLRegex
+		 * A regular expression that matches whether an URL is from anywhere on the repository site.
+		 */
+		protected Regex isSiteURLRegex;
+
+		/* var: isProjectURLRegex
+		 * A regular expression that matches whether an URL is to a project page on the repository site.
+		 */
+		protected Regex isProjectURLRegex;
+
+		/* var: sourceURLTemplate_WithBranch
+		 * A repository source URL template string used when a branch name is defined, or null if branch names don't appear
+		 * in URLs.
+		 */
+		protected string sourceURLTemplate_WithBranch;
+
+		/* var: sourceURLTemplate_WithoutBranch
+		 * A repository source URL template string used when a branch name is not defined, or null if branches must appear in
+		 * URLs.
+		 */
+		protected string sourceURLTemplate_WithoutBranch;
+
+		/* var: exampleProjectURL
+		 * A string to use as an example project URL in error messages, such as "https://github.com/username/projectname/".
+		 */
+		protected string exampleProjectURL;
+
+		}
+	}
---
+++ Engine/Source/Config/RepositorySites.cs
@@ -0,0 +1,189 @@
+﻿/*
+ * Struct: CodeClear.NaturalDocs.Engine.Config.RepositorySites
+ * ____________________________________________________________________________
+ *
+ * A static class storing information about all known repository sites.
+ *
+ */
+
+// This file is part of Natural Docs, which is Copyright © 2003-2025 Code Clear LLC.
+// Natural Docs is licensed under version 3 of the GNU Affero General Public License (AGPL)
+// Refer to License.txt for the complete details
+
+
+using System;
+using System.Text.RegularExpressions;
+
+
+namespace CodeClear.NaturalDocs.Engine.Config
+	{
+	public static partial class RepositorySites
+		{
+
+		// Group: Functions
+		// __________________________________________________________________________
+
+
+		static RepositorySites ()
+			{
+			var gitHub = new RepositorySite (
+				name: "GitHub",
+				isSiteURLRegex: IsGitHubURLRegex(),
+				isProjectURLRegex: IsGitHubProjectURLRegex(),
+				sourceURLTemplate_WithBranch: RepositorySubstitutions.ProjectURL +
+																"/blob/" + RepositorySubstitutions.Branch + "/" +
+																RepositorySubstitutions.FilePath +
+																"#L" + RepositorySubstitutions.LineNumber,
+				sourceURLTemplate_WithoutBranch: RepositorySubstitutions.ProjectURL +
+																	"/blob/master/" + // Will redirect to the default branch even if it isn't called "master"
+																	RepositorySubstitutions.FilePath +
+																	"#L" + RepositorySubstitutions.LineNumber,
+				exampleProjectURL: "https://github.com/UserName/ProjectName"
+				);
+
+			var gitLab = new RepositorySite (
+				name: "GitLab",
+				isSiteURLRegex: IsGitLabURLRegex(),
+				isProjectURLRegex: IsGitLabProjectURLRegex(),
+				sourceURLTemplate_WithBranch: RepositorySubstitutions.ProjectURL +
+																"/-/blob/" + RepositorySubstitutions.Branch + "/" +
+																RepositorySubstitutions.FilePath +
+																"#L" + RepositorySubstitutions.LineNumber,
+				sourceURLTemplate_WithoutBranch: null, // Not possible to omit the branch or redirect to the default branch
+				exampleProjectURL: "https://gitlab.com/UserName/ProjectName"
+				);
+
+			var codeberg = new RepositorySite (
+				name: "Codeberg",
+				isSiteURLRegex: IsCodebergURLRegex(),
+				isProjectURLRegex: IsCodebergProjectURLRegex(),
+				sourceURLTemplate_WithBranch: RepositorySubstitutions.ProjectURL +
+																"/src/branch/" + RepositorySubstitutions.Branch + "/" +
+																RepositorySubstitutions.FilePath +
+																"#L" + RepositorySubstitutions.LineNumber,
+				sourceURLTemplate_WithoutBranch: RepositorySubstitutions.ProjectURL +
+																	"/src/" +
+																	RepositorySubstitutions.FilePath +
+																	"#L" + RepositorySubstitutions.LineNumber,
+				exampleProjectURL: "https://codeberg.org/UserName/ProjectName"
+				);
+
+			var gitea = new RepositorySite (
+				name: "Gitea",
+				isSiteURLRegex: IsGiteaURLRegex(),
+				isProjectURLRegex: IsGiteaProjectURLRegex(),
+				sourceURLTemplate_WithBranch: RepositorySubstitutions.ProjectURL +
+																"/src/branch/" + RepositorySubstitutions.Branch + "/" +
+																RepositorySubstitutions.FilePath +
+																"#L" + RepositorySubstitutions.LineNumber,
+				sourceURLTemplate_WithoutBranch: RepositorySubstitutions.ProjectURL +
+																	"/src/" +
+																	RepositorySubstitutions.FilePath +
+																	"#L" + RepositorySubstitutions.LineNumber,
+				exampleProjectURL: "https://gitea.com/UserName/ProjectName"
+				);
+
+			knownSites = new RepositorySite[] {  gitHub, gitLab, codeberg, gitea  };
+			}
+
+
+		/* Function: FromName
+		 * Returns a <RepositorySite> from the passed name, or null if it is unrecognized.
+		 */
+		public static RepositorySite FromName (string name)
+			{
+			foreach (var knownSite in knownSites)
+				{
+				if (string.Compare(name, knownSite.Name, StringComparison.InvariantCultureIgnoreCase) == 0)
+					{  return knownSite;  }
+				}
+
+			return null;
+			}
+
+
+		/* Function: FromURL
+		 * Returns a <RepositorySite> associated with the passed URL, or null if it is unrecognized.
+		 */
+		public static RepositorySite FromURL (string url)
+			{
+			foreach (var knownSite in knownSites)
+				{
+				if (knownSite.IsSiteURL(url))
+					{  return knownSite;  }
+				}
+
+			return null;
+			}
+
+
+
+		// Group: Variables
+		// __________________________________________________________________________
+
+		private static RepositorySite[] knownSites;
+
+
+
+		// Group: Regular Expressions
+		// __________________________________________________________________________
+
+
+		/* Regex: IsGitHubURLRegex
+		 * Will match if the string is a GitHub URL of any kind.
+		 */
+		[GeneratedRegex("""^https?://(?:www\.)?github\.com(?:/|$)""",
+								  RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+		static internal partial Regex IsGitHubURLRegex();
+
+		/* Regex: IsGitHubProjectURLRegex
+		 * Will match if the string is a GitHub project URL.
+		 */
+		[GeneratedRegex("""^https?://(?:www\.)?github\.com/[^/?#]+/[^/?#]+/?$""",
+								  RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+		static internal partial Regex IsGitHubProjectURLRegex();
+
+		/* Regex: IsGitLabURLRegex
+		 * Will match if the string is a GitLab URL of any kind.
+		 */
+		[GeneratedRegex("""^https?://(?:www\.)?gitlab\.com(?:/|$)""",
+								  RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+		static internal partial Regex IsGitLabURLRegex();
+
+		/* Regex: IsGitLabProjectURLRegex
+		 * Will match if the string is a GitLab project URL.
+		 */
+		[GeneratedRegex("""^https?://(?:www\.)?gitlab\.com/[^/?#]+/[^/?#]+/?$""",
+								  RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+		static internal partial Regex IsGitLabProjectURLRegex();
+
+		/* Regex: IsCodebergURLRegex
+		 * Will match if the string is a Codeberg URL of any kind.
+		 */
+		[GeneratedRegex("""^https?://(?:www\.)?codeberg\.org(?:/|$)""",
+								  RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+		static internal partial Regex IsCodebergURLRegex();
+
+		/* Regex: IsCodebergProjectURLRegex
+		 * Will match if the string is a Codeberg project URL.
+		 */
+		[GeneratedRegex("""^https?://(?:www\.)?codeberg\.org/[^/?#]+/[^/?#]+/?$""",
+								  RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+		static internal partial Regex IsCodebergProjectURLRegex();
+
+		/* Regex: IsGiteaURLRegex
+		 * Will match if the string is a Gitea URL of any kind.
+		 */
+		[GeneratedRegex("""^https?://(?:www\.)?gitea\.com(?:/|$)""",
+								  RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+		static internal partial Regex IsGiteaURLRegex();
+
+		/* Regex: IsGiteaProjectURLRegex
+		 * Will match if the string is a Gitea project URL.
+		 */
+		[GeneratedRegex("""^https?://(?:www\.)?gitea\.com/[^/?#]+/[^/?#]+/?$""",
+								  RegexOptions.Singleline | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant)]
+		static internal partial Regex IsGiteaProjectURLRegex();
+
+		}
+	}
