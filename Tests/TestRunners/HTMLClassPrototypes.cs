f48dc83caaf7391b0c5212f3610ee8faf9d402de
Greg Valure <gregvalure@naturaldocs.org>
2026-07-08 00:07:12 -0400

    Add topic titles to class prototype test output.

--- Tests/TestRunners/HTMLClassPrototypes.cs
+++ Tests/TestRunners/HTMLClassPrototypes.cs
@@ -24,7 +24,8 @@ namespace CodeClear.NaturalDocs.Tests.TestRunners
 			return ExtractHTML(input,
 										 tagName: "div",
 										 className: "NDClassPrototype",
-										 reformatHTML: true);
+										 reformatHTML: true,
+										 addTopicTitles: true);
 			}
 
 		}
