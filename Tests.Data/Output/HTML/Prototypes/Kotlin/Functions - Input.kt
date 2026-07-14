90f4a60e7f72f070fda10e3502648953109ec3c0
Greg Valure <gregvalure@naturaldocs.org>
2026-07-14 15:57:29 -0400

    Add tests for :this() and :super() calls Kotlin constructor prototypes.

---
+++ Tests.Data/Output/HTML/Prototypes/Kotlin/Functions - Input.kt
@@ -0,0 +1,14 @@
+
+// Function: NormalFunction
+fun NormalFunction(parameter: Int): Int
+	{  }
+
+// Function: constructor
+// The base constructor call should not be included in the output.
+constructor(parameter: Int) : super(parameter)
+	{  }
+
+// Function: constructor
+// The other constructor call should not be included in the output.
+constructor(parameter: Int) : this(parameter)
+	{  }
